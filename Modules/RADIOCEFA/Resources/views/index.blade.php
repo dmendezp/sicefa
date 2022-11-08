7a2e2289a2c1261585c722e71a493e73353d3b1e
Mariak19poyo <looney2407@gmail.com>
2022-11-08 18:27:17 -0500

    Creacion modulo
    Creacion del modulo RADIOCEFA

---
+++ Modules/RADIOCEFA/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('radiocefa::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('radiocefa.name') !!}
+    </p>
+@endsection
