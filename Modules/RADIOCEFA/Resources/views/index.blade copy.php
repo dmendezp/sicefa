3f5ed3c3437dbd18b3fa533730bf8629c72b6b39
Mariak19poyo <looney2407@gmail.com>
2022-11-09 06:15:10 -0500

    commit radio
    insertada la radio virtual

---
+++ Modules/RADIOCEFA/Resources/views/index.blade copy.php
@@ -0,0 +1,6 @@
+@extends('radiocefa::layouts.master')
+{{-- dentro de esta etiqueta va el contenido de la pagina --}}
+@section('content')
+    
+@endsection
+{{-- dentro de esta etiqueta va el contenido de la pagina /--}}
--- Modules/RADIOCEFA/Resources/views/index.blade.php
+++ Modules/RADIOCEFA/Resources/views/index.blade.php
@@ -1,9 +1,13 @@
 @extends('radiocefa::layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
 
-    <p>
-        This view is loaded from module: {!! config('radiocefa.name') !!}
-    </p>
+<div class="content">
+    <div class="header">
+        <img src="{{asset('radi__cefa/headerRadio.jpg')}}" style="bacgraund-color:rgb(194, 194, 194, 0.5)">
+    </div>
+</div>
+
+<iframe src="https://zeno.fm/player/radio-cefa" width="768" height="600" frameborder="0" scrolling="no"></iframe><a href="https://zeno.fm/" target="_blank" style="display: block; font-size: 0.9em; line-height: 10px;">A Zeno.FM Station</a>
+
 @endsection
