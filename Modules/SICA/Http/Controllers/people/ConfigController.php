92a0c4f38720d76913bbed0d310dc93cd2b3ef5c
GUEVARA0331 <jdguevara01@misena.edu.co>
2022-11-19 18:12:25 -0500

    Start of event module

--- Modules/SICA/Http/Controllers/people/ConfigController.php
+++ Modules/SICA/Http/Controllers/people/ConfigController.php
@@ -5,13 +5,15 @@ namespace Modules\SICA\Http\Controllers\people;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\SICA\Entities\Event;
 
 class ConfigController extends Controller
 {
 
     public function config(){
-        $data = ['title'=>trans('sica::menu.Config')];
-        return view('sica::admin.people.config.home',$data);        
-    } 
+        $events = Event::all();
+        $data = ['title'=>trans('sica::menu.Config'), 'events'=>$events];
+        return view('sica::admin.people.config.home',$data);
+    }
 
 }
