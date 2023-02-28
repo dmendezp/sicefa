ae118f1db79d0a8ffa054515677f1cf1f67d5be3
Sabogal22 <nsabogalgaitan@gmail.com>
2023-02-28 14:24:45 -0500

    Add Page DataBase

---
+++ Modules/CEFAMAPS/Entities/Page.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\CEFAMAPS\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\CEFAMAPS\Entities\Environment;
+
+class page extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [];
+    protected $dates ['deleted_at'];
+    protected $hidden = ['created_at','updated_at'];
+
+    public function environments(){
+        return $this->belongsTo(Environment::class);
+    }
+}
--- Modules/SICA/Entities/Environment.php
+++ Modules/SICA/Entities/Environment.php
@@ -5,6 +5,7 @@ namespace Modules\SICA\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Modules\CEFAMAPS\Entities\Coordinate;
+use Modules\CEFAMAPS\Entities\Page;
 
 class Environment extends Model
 {
@@ -15,5 +16,9 @@ class Environment extends Model
 
     public function coordinates(){
         return $this->hasMany(Coordinate::class);
+    }
+
+    public function pages(){
+        return $this->hasMany(Page::class);
     }
 }
