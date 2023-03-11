b3600aed065556179eb2304dc6fe30e0b7e4245a
GabrielGG024 <gtgabriel78@gmail.com>
2023-03-11 16:04:45 -0500

    hro

--- Modules/RADIOCEFA/Resources/views/layouts/partials/hero.blade.php
+++ Modules/RADIOCEFA/Resources/views/layouts/partials/hero.blade.php
@@ -1,18 +1,11 @@
 <!-- ======= Hero Section ======= -->
-<section class="">
-
-    <div class="container">
+<section class="d-lg-flex align-items-sm-start">
+    <img src="{{asset('radi__cefa/headerRadio.jpg')}}" class="vw-100 position-fixed  ">
+    <div class="container ">
       <div class="row">
-    <img src="{{asset('radi__cefa/headerRadio.jpg')}}" class="position-relative w-25">
         <div class=" d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Radio CEFA</h1>
           <h2><p>Radio Institucional </p><p>"La Angostura"</p></h2>
-          <div class="d-flex justify-content-center justify-content-lg-start">
-            <!--video de prsentacion del sena--->
-            <a href="#" class="btn-get-started scrollto glightbox"><i class="bi bi-play-circle"></i></a>
-            
-            <!--<a href="#" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>-->
-          </div>
         </div>
         
       </div>
