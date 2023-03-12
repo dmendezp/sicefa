d310a9c529fada0a6a8e52de7f7efae46c2da802
GabrielGG024 <gtgabriel78@gmail.com>
2023-03-12 01:11:27 -0500

    hero terminado

--- Modules/RADIOCEFA/Resources/views/layouts/partials/hero.blade.php
+++ Modules/RADIOCEFA/Resources/views/layouts/partials/hero.blade.php
@@ -1,12 +1,12 @@
 <!-- ======= Hero Section ======= -->
-  <section  class="d-flex align-items-center bg-success vw-100">
+  <section  class="d-flex align-items-center vw-100 h-75" style="background-color: #719F82;">
 
-    <div class="container">
+    <div class="container" style="margin-left: 0px; padding: 0px;">
       <div class="row">
-        <div class="col-lg-8 " data-aos="fade-down-right" data-aos-delay="200">
-          <img src="{{asset('radi__cefa/headerRadio.jpg')}}" class="img-fluid animated zoom vw-100" >
+        <div class="col-8 " data-aos="fade-down-right" data-aos-delay="200">
+          <img src="{{asset('radi__cefa/headerRadio.jpg')}}" class="img-fluid vw-100">
         </div>
-        <div class="col-lg-4 d-flex flex-column justify-content-center pt-4 pt-lg-0 " data-aos="fade-up" data-aos-delay="200">
+        <div class="col-lg-4 d-flex flex-column justify-content-center pt-4 pt-lg-0 text-light" data-aos="fade-up" data-aos-delay="200">
           <h1>Sistema integrado del CEFA</h1>
           <h2><p>Centro de Formación Agroindustrial</p><p>"La Angostura"</p></h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
