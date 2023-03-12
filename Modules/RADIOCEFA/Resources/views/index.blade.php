0b90362349ce8d8849fb32192544318cc7160beb
Mariak19poyo <looney2407@gmail.com>
2023-03-12 00:56:42 -0500

    fotoscards
    fotod de las cards

--- Modules/RADIOCEFA/Resources/views/index.blade.php
+++ Modules/RADIOCEFA/Resources/views/index.blade.php
@@ -13,21 +13,21 @@
 
 <div class ="row" >
     <div class="card col-4 card-align-center" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="{{asset('radi__cefa/senalaire.jpg') }}" class="card-img-top" alt="...">
         <div class="card-body">
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
 
       <div class="card col-4 card-align-center" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="{{asset('radi__cefa/senalaire.jpg') }}" class="card-img-top" alt="...">
         <div class="card-body">
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
 
       <div class="card col-4 card-align-center" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="{{asset('radi__cefa/senalaire.jpg') }}" class="card-img-top" alt="...">
         <div class="card-body">
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
