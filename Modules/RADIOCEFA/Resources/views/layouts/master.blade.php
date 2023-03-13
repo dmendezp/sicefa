5bb7e6bf0413db6697b46374f629af36dcbc422d
GabrielGG024 <gtgabriel78@gmail.com>
2023-03-12 23:47:58 -0500

    incluir cambios

--- Modules/RADIOCEFA/Resources/views/layouts/master.blade.php
+++ Modules/RADIOCEFA/Resources/views/layouts/master.blade.php
@@ -10,11 +10,40 @@
         {{-- Laravel Mix - CSS File --}}
        {{-- <link rel="stylesheet" href="{{ mix('css/radiocefa.css') }}"> --}}
 
+{{-- guardar dentro de head en partials --}}
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
+{{-- end --}}
+
+
     </head>
     <body>
         
         @yield('content')
         @include('layouts/partials/scripts')
+
+{{-- guardar en in scrip dentro de partials  --}}
+        <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../../bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+{{-- end --}}
+
 
         {{-- Laravel Mix - JS File --}}
         {{-- <script src="{{ mix('js/radiocefa.js') }}"></script> --}}
