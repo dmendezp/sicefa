d9aefb740b25d184bacc9d005f8596600da57e31
SrManuel-1 <stevenlievano7575@gmail.com>
2023-03-28 15:49:11 -0500

    Reparación del tooltip

--- Modules/PTVENTA/Resources/views/products/index.blade.php
+++ Modules/PTVENTA/Resources/views/products/index.blade.php
@@ -137,12 +137,6 @@
 @endsection
 
 @section('scripts')
-<script>
-    // Activa el plugin tooltip de Bootstrap
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  </script>
+
   
 @endsection
