5be5c1293732080f33b1d978b36cfd6d72bea0eb
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-04-07 11:16:33 -0500

    Generar imagen faker para seeder de Element en PTVENTA

--- Modules/PTVENTA/Database/Seeders/ElementTableSeeder.php
+++ Modules/PTVENTA/Database/Seeders/ElementTableSeeder.php
@@ -2,14 +2,12 @@
 
 namespace Modules\PTVENTA\Database\Seeders;
 
-use Faker\Factory;
-use Faker\Image;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Storage;
 use Modules\SICA\Entities\Category;
 use Modules\SICA\Entities\KindOfPurchase;
 use Modules\SICA\Entities\Element;
 use Modules\SICA\Entities\MeasurementUnit;
+use Modules\SICA\Database\factories\ElementFactory;
 
 class ElementTableSeeder extends Seeder
 {
@@ -20,35 +18,28 @@ class ElementTableSeeder extends Seeder
      */
     public function run()
     {
-        $category = Category::updateOrCreate(['name' => 'Lácteos'],['kind_of_property' => 'Bodega']);
-        $kind_of_purchase = KindOfPurchase::updateOrCreate(['name' => 'Producción de centro'],['description' => 'Elementos de consumo que provienen de producción de centro']);
-        $measurement_unit = MeasurementUnit::updateOrCreate(['name' => 'Unidad'],[
+        $category = Category::updateOrCreate(['name' => 'Lácteos'],['kind_of_property' => 'Bodega']); // Registrar o registrar Categoría
+        $kind_of_purchase = KindOfPurchase::updateOrCreate(['name' => 'Producción de centro'],['description' => 'Elementos de consumo que provienen de producción de centro']); // Actualizar o registrar Tipo de compra
+        $measurement_unit = MeasurementUnit::updateOrCreate(['name' => 'Unidad'],[ // Actualizar o crear Unidad de medida
             'minimum_unit_measure' => 'Unidad',
             'conversion_factor' => 1
         ]);
-        Element::updateOrCreate(['name' => 'Yogurt de mora x 225ml'],[
+        $e = new Element(['name' => 'Yogurt de mora x 225ml']);  // Instanciar Elemento y definir su Nombre
+        Element::updateOrCreate(['name' => $e->name],[  // Actualizar o registrar Elemento
             'measurement_unit_id' => $measurement_unit->id,
             'description' => 'Delicioso yogurt con sabor a Mora y trocitos de fruta',
             'kind_of_purchase_id' => $kind_of_purchase->id,
             'category_id' => $category->id,
-            'image' => $this->generateImage()
+            'image' => ElementFactory::new()->withName($e->slug)->make()->image  // Generar imagen de acuerdo al slug generado por la instacia del Elemento al establecer un valor en el atributo na🤼‍♂️
         ]);
 
-        $category = Category::updateOrCreate(['name' => 'Repostería'],['kind_of_property' => 'Bodega']);
-        Element::updateOrCreate(['name' => 'Dona de chocolate x 50gr'],[
+        $e = new Element(['name' => 'Dona de chocolate x 50gr']); // Instanciar Elemento y definir su Nombre
+        Element::updateOrCreate(['name' => $e->name],[ // Actualizar o registrar Elemento
             'measurement_unit_id' => $measurement_unit->id,
             'description' => 'Deliciosas donas de chocolate y crispy',
             'kind_of_purchase_id' => $kind_of_purchase->id,
             'category_id' => $category->id,
-            'image' => $this->generateImage()
+            'image' => ElementFactory::new()->withName($e->slug)->make()->image // // Generar imagen de acuerdo al slug generado por la instacia del Elemento al establecer un valor en el atributo na🤼‍♂️
         ]);
-    }
-
-    private function generateImage(){ // Generar imagen utilizando faker (se guarda en el directorio => storage/app/modules/sica/elements)
-        $faker = Factory::create();
-        $imagePath = 'modules/sica/elements/' . uniqid() . '.jpg'; // Definir la ruta y el nombre del archivo de imagen
-        $imageContents = file_get_contents($faker->imageUrl(640, 480)); // Obtener los bytes de la imagen desde una URL generada por Faker
-        Storage::put($imagePath, $imageContents); // Guardar la imagen en el sistema de archivos
-        return $imagePath;
     }
 }
--- Modules/PTVENTA/Database/Seeders/PTVENTADatabaseSeeder.php
+++ Modules/PTVENTA/Database/Seeders/PTVENTADatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\PTVENTA\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PTVENTADatabaseSeeder extends Seeder
 {
@@ -14,8 +15,12 @@ class PTVENTADatabaseSeeder extends Seeder
     public function run()
     {
 
+        DB::beginTransaction(); // Iniciar transacción
+
         $this->call(AppTableSeeder::class); // llamar seeder de aplicación PTVENTA
         $this->call(ElementTableSeeder::class); // llamar seeder de elementos
+
+        DB::commit(); // Finalizar transación
 
     }
 }
