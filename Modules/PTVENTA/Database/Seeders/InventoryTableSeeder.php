a536f8b829f7477a1e046a237a0c5418a801b88b
nelsygomez11 <moralesnelsygomez11@gmail.com>
2023-04-17 10:32:15 -0500

    PTVENTA - Configuracion de seeders de inventory

--- Modules/PTVENTA/Database/Seeders/InventoryTableSeeder.php
+++ Modules/PTVENTA/Database/Seeders/InventoryTableSeeder.php
@@ -15,11 +15,28 @@ class InventoryTableSeeder extends Seeder
      */
     public function run()
     {
-
-        
-    
+       Model:unguard();
 
         // $this->call("OthersTableSeeder");
+        invetories::create([
+
+            'person_id' => '1',
+            'werehouse_id' => '1',
+            'element_id' => '1',
+            'destination' => 'Producción',
+            'description' => 'Texto largo',
+            'price' => 'Número entero',
+            'amount' => 'Número entero',
+            'stock' => 'Número entero',
+            'production_date' => 'Fecha',
+            'lot_number' => 'Número entero',
+            'expiration_date' => 'Fecha',
+            'state' => 'Disponible',
+            'mark' => 'Texto corto',
+            'inventory_code' => 'Número entero no asignado',
+
+
+        ]);
 
         
     }
