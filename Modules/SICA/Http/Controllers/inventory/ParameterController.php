9b594117d12a3233d0393a00923a616890ee8477
SrManuel-1 <stevenlievano7575@gmail.com>
2023-04-18 20:29:49 -0500

    SICA - CRUD de Categorías

--- Modules/SICA/Http/Controllers/inventory/ParameterController.php
+++ Modules/SICA/Http/Controllers/inventory/ParameterController.php
@@ -15,14 +15,14 @@ class ParameterController extends Controller
         return view('sica::admin.inventory.parameters.index',$data);
     }
 
-    public function editCategoryGet(){
-        return view('sica::admin.inventory.parameters.category.edit');
+    public function addCategoryGet(){
+        return view('sica::admin.inventory.parameters.category.add');
     }
 
-    public function editCategoryPost(Request $request){
-        $c = new Category();
+    public function addCategoryPost(Request $request){
+        $c = new Category;
         $c->name = e($request->input('name'));
-        $c->kind_of_property = e($request->input('kind_of_property'));
+        $c->kind_of_property = 'Bodega';
         $card = 'card-category';
         if($c->save()){
             $icon = 'success';
@@ -30,6 +30,43 @@ class ParameterController extends Controller
         }else{
             $icon = 'error';
             $message_config = 'No se pudo actualizar la categoria.';
+        }
+        return back()->with(['card'=>$card, 'icon'=>$icon, 'message_config'=>$message_config]);
+    }
+
+    public function editCategoryGet($id){
+        $c = Category::find($id);
+        return view('sica::admin.inventory.parameters.category.edit',compact('category'));
+    }
+
+    public function editCategoryPost(Request $request){
+        $c = Category::findOrFail($request->input('id'));
+        $c->name = e($request->input('name'));
+        $card = 'card-category';
+        if($c->save()){
+            $icon = 'success';
+            $message_config = 'Categoria actualizada exitosamente.';
+        }else{
+            $icon = 'error';
+            $message_config = 'No se pudo actualizar la categoria.';
+        }
+        return back()->with(['card'=>$card, 'icon'=>$icon, 'message_config'=>$message_config]);
+    }
+
+    public function deleteCategoryGet($id){
+        $c = Category::find($id);
+        return view('sica::admin.inventory.parameters.category.delete',compact('category'));
+    }
+
+    public function deleteEpsPost(Request $request){
+        $c = Category::findOrFail($request->input('id'));
+        $card = 'card-category';
+        if($c->delete()){
+            $icon = 'success';
+            $message_config = 'Categoría eliminada exitosamente.';
+        }else{
+            $icon = 'error';
+            $message_config = 'No se pudo eliminar la categoría.';
         }
         return back()->with(['card'=>$card, 'icon'=>$icon, 'message_config'=>$message_config]);
     }
--- Modules/SICA/Resources/views/admin/inventory/parameters/category/delete.blade.php
+++ Modules/SICA/Resources/views/admin/inventory/parameters/category/delete.blade.php
@@ -1,19 +1,23 @@
 <div id="content-config">
     <div class="modal-header py-2">
         <h5 class="modal-title" id="exampleModalLabel">
-            <b>Actualizar categoria</b>
+            <b>¿Desea eliminar la siguiente categoría?</b>
         </h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
-    {!! Form::open(['route'=>'sica.admin.inventory.parameters.category.edit', 'method'=>'POST', 'id'=>'form-config']) !!}
-        <div class="modal-body px-4 pt-0">
-                @include('sica::admin.inventory.parameters.category.form')
+    {!! Form::open(['route'=>'sica.admin.inventory.parameters.category.delete', 'method'=>'POST', 'id'=>'form-config']) !!}
+        <div class="modal-body px-4 pt-3" style="font-size: 20px;">
+            {!! Form::hidden('id', $c->id) !!}
+            <div class="row">
+                <div class="col-4 text-right"><b>Nombre: </b></div>
+                <div class="col">{{ $c->name }}</div>
+            </div>
         </div>
         <div class="modal-footer py-1">
                 <button type="button" class="btn btn-secondary btn-md py-0" data-dismiss="modal">Cancelar</button>
-                {!! Form::submit('Actualizar', ['class'=>'btn btn-success btn-md py-0']) !!}
+                {!! Form::submit('Eliminar', ['class'=>'btn btn-danger btn-md py-0']) !!}
         </div>
     {!! Form::close() !!}
 </div>
