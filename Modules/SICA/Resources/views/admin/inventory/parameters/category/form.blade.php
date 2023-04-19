9eeeeb5aff534add3a6aef84318b570137c57d30
SrManuel-1 <stevenlievano7575@gmail.com>
2023-04-18 21:00:26 -0500

    SICA - Agregar label a Tipo de propiedad en el form de Categorías

--- Modules/SICA/Resources/views/admin/inventory/parameters/category/form.blade.php
+++ Modules/SICA/Resources/views/admin/inventory/parameters/category/form.blade.php
@@ -9,6 +9,10 @@
         </span>
     </div>
     {!! Form::text('name', isset($c) ? $c->name : null, ['class' => 'form-control', 'required', 'onkeyup'=>"mayus(this)"]) !!}
+</div>
+
+{!! Form::label('kind_of_property', 'Tipo de Propiedad:', ['class' => 'mt-3']) !!}
+<div class="input-group">
     <div class="input-group-prepend">
         <span class="input-group-text">
             <i class="far fa-keyboard"></i>
