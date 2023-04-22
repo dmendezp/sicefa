ad944f3105d30792b4f4ea66d44bf5169d88d9c2
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-04-22 11:08:37 -0500

    PTVENTA - Crear seeder para inventories

--- Modules/PTVENTA/Database/Seeders/InventoryTableSeeder.php
+++ Modules/PTVENTA/Database/Seeders/InventoryTableSeeder.php
@@ -4,9 +4,9 @@ namespace Modules\PTVENTA\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Modules\SICA\Entities\Inventory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
+use Modules\SICA\Entities\Element;
+use Modules\SICA\Entities\Person;
+use Modules\SICA\Entities\Warehouse;
 
  class InventoryTableSeeder extends Seeder
 {
@@ -18,22 +18,26 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
     public function run()
 
     {
-        Inventory::create([
 
-            'person_id' => 1,
-            'warehouse_id' => 1,
-            'element_id' => 1,
+        $number_inventories = 50; // Definir la cantidad de registros de inventarios de prueba
+
+        // Crear registro de inventario predeterminado por
+        Inventory::create([
+            'person_id' => Person::where('document_number',52829681)->firstOrFail()->id,
+            'warehouse_id' => Warehouse::where('name','Punto de venta')->firstOrFail()->id,
+            'element_id' => Element::where('name','Yogurt de mora x 225ml')->firstOrFail()->id,
             'destination' => 'Producción',
-            'description' => null,
             'price' => 1200,
-            'amount' => 6,
+            'amount' => 18,
             'stock' => 12,
-            'production_date' => '2023-04-17',
+            'production_date' => '2023-03-27',
             'lot_number' => 2112,
-            'expiration_date' => '2023-04-24',
-            'state' => 'Disponible',
-            'mark' => 'CEFA',
-            'inventory_code' => null,
-        ]);    
+            'expiration_date' => '2023-04-04',
+            'mark' => 'CEFA'
+        ]);
+
+        // Generar inventarios de prueba de acuerdo a la cantidad requerida
+        Inventory::factory()->count($number_inventories)->create();
+
     }
 }
