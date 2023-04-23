91b21ea658d75aa78e44ec21b1a4a8d4d708fcb6
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-04-22 23:42:57 -0500

    PTVENTA - Galería de imágenes (primer avance)

--- Modules/PTVENTA/Resources/views/element/gallery.blade.php
+++ Modules/PTVENTA/Resources/views/element/gallery.blade.php
@@ -1,8 +1,43 @@
 @extends('ptventa::layouts.master')
 
 @section('head')
-    {{-- PhotoSwipe-5.3.7 --}}
-    <link rel="stylesheet" href="{{ asset('PhotoSwipe-5.3.7/dist/photoswipe.css') }}">
+    <style>
+        .card-image {
+            height: 200px;
+            width: 300px;
+            background-size: cover !important;
+            color: white;
+            position: relative;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }
+
+        .card-image:hover {
+            opacity: 0.8;
+        }
+
+        .card-category {
+            border-start-start-radius: 5px;
+            border-start-end-radius: 5px;
+            padding-left: 5px;
+            background-color:rgba(0, 0, 0, 0.386);
+            width: 100%;
+            position: absolute;
+            font-size: 15px;
+        }
+
+        .card-description {
+            border-end-start-radius: 5px;
+            border-end-end-radius: 5px;
+            bottom: 0px;
+            width: 100%;
+            height: 30px;
+            position: absolute;
+            padding-inline: 10px;
+            background-color: OliveDrab;
+            font-size: 14px;
+        }
+    </style>
 @endsection
 
 @section('breadcrumb')
@@ -14,25 +49,30 @@
 @section('content')
     <div class="card card-success card-outline">
         <div class="card-body">
-            <div class="pswp-gallery" id="my-gallery">
-                @foreach ($elements as $e)
-                    <a href="{{ asset($e->image) }}" data-pswp-width="60" data-pswp-height="40">
-                        <img src="{{ asset($e->image) }}" alt="{{ $e->name }}"/>
-                    </a>
-                @endforeach
-            </div>
+
+                <div class="d-inline-flex">
+                    <div class="row  justify-content-center mx-auto">
+                        @foreach ($elements as $e)
+                            <div class="col-auto">
+                                <div class="card-image" style="background: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('{{ asset($e->image) }}'), url('{{ asset('modules/sica/images/sinImagen.png') }}');">
+                                    <div class="card-category text-center"><strong>{{ $e->name }}</strong></div>
+                                    <div class="card-description">
+                                        <p class="mt-1">
+                                            {{ $e->category->name }}
+                                            <a href="#" class="text-light float-right">
+                                                <i class="fa-solid fa-pen-to-square fs-6"></i>
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+
         </div>
     </div>
 @endsection
 
 @section('scripts')
-    <script src="{{ asset('PhotoSwipe-5.3.7/dist/photoswipe-lightbox.esm.js') }}"></script>
-    <script>
-        const lightbox = new PhotoSwipeLightbox({
-            gallery: '#my-gallery',
-            children: 'a',
-            pswpModule: () => import('{{ asset("PhotoSwipe-5.3.7/dist/photoswipe.esm.js") }}')
-        });
-        lightbox.init();
-    </script>
 @endsection
--- Modules/PTVENTA/Resources/views/layouts/partials/head.blade.php
+++ Modules/PTVENTA/Resources/views/layouts/partials/head.blade.php
@@ -4,8 +4,8 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>PTVENTA | {{ $view['titlePage'] }}</title>
 
-<!-- Font Awesome Icons -->
-<link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}"> {{-- Iconos usados desde la plantilla adminLTE --}}
+<!-- Font Awesome Icons version 6 -->
+<link rel="stylesheet" href="{{ asset('fontawesome6/css/all.min.css') }}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.css') }}"> {{-- Estilos principales de adminLTE --}}
 {{-- Link CSS --}}
