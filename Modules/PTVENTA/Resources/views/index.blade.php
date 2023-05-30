2fbd7c84c2d220fa37b36e92ca191ea30e3813a9
SrManuel-1 <stevenlievano7575@gmail.com>
2023-05-29 20:26:49 -0500

    PTVENTA - Implementación de AOS (Animate On Scroll)

--- Modules/PTVENTA/Resources/views/index.blade.php
+++ Modules/PTVENTA/Resources/views/index.blade.php
@@ -1,16 +1,21 @@
 @extends('ptventa::layouts.master')
 
+@push('head')
+    <!-- Estilos de la galería de imágenes -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+@endpush
+
 @push('breadcrumbs')
     <li class="breadcrumb-item active">{{ trans('ptventa::mainPage.Main page')}}</li>
 @endpush
 
 @section('content')
     <h1 class="display-3">{{ trans('ptventa::mainPage.Welcome to point of sale')}}</h1>
-    <p>{{ trans('ptventa::mainPage.Point of sale is a productive unit belonging to the Agroindustrial Training Center "La Angostura", here are offered the different products that are manufactured in this center, we have products that come directly from the field as well as those that are processed in the agribusiness sector.')}}</p>
+    <p data-aos="fade-down">{{ trans('ptventa::mainPage.Point of sale is a productive unit belonging to the Agroindustrial Training Center "La Angostura", here are offered the different products that are manufactured in this center, we have products that come directly from the field as well as those that are processed in the agribusiness sector.')}}</p>
 
     <div class="row">
         <div class="col-md-6">
-            <div class="card text-center mb-3 shadow-sm">
+            <div class="card text-center mb-3 shadow-sm" data-aos="fade-up" data-aos-duration="3000">
                 <div class="card-body">
                     <div class="d-flex align-items-center justify-content-center">
                         <div>
@@ -56,7 +61,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card text-center mb-3 shadow-sm">
+            <div class="card text-center mb-3 shadow-sm" data-aos="fade-up" data-aos-duration="3000">
                 <div class="card-body">
                     <hr class="featurette-divider">
                     <div class="row featurette align-items-center">
@@ -75,5 +80,9 @@
         </div>
 @endsection
 
-@section('scripts')
-@endsection
+@push('scripts')
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+      </script>
+@endpush
