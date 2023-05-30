8f7890fa60d73f626a56faa7dd8260f2fb7d5d4f
SrManuel-1 <stevenlievano7575@gmail.com>
2023-05-29 20:49:00 -0500

    PTVENTA - Implementación de AOS (Animate On Scroll) de manera local

--- Modules/PTVENTA/Resources/views/index.blade.php
+++ Modules/PTVENTA/Resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @push('head')
     <!-- Estilos de la galería de imágenes -->
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="{{asset('modules/ptventa/libs/aos2.3.1/dist/aos.css')}}" rel="stylesheet">
 @endpush
 
 @push('breadcrumbs')
@@ -81,8 +81,8 @@
 @endsection
 
 @push('scripts')
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="{{asset('modules/ptventa/libs/aos2.3.1/dist/aos.js')}}"></script>
     <script>
         AOS.init();
-      </script>
+    </script>
 @endpush
