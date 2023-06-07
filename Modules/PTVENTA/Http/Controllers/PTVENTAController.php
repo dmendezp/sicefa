f44ddef9d7a4618252066b1bcf7de5fa31b3e97b
SrManuel-1 <stevenlievano7575@gmail.com>
2023-06-06 19:17:07 -0500

    PTVENTA - Internacionalización de Acerca de

--- Modules/PTVENTA/Http/Controllers/PTVENTAController.php
+++ Modules/PTVENTA/Http/Controllers/PTVENTAController.php
@@ -8,17 +8,17 @@ class PTVENTAController extends Controller
 {
 
     public function index(){
-        $view = ['titlePage'=>'Inicio', 'titleView'=>trans('ptventa::mainPage.Main page')];
+        $view = ['titlePage'=>trans('ptventa::mainPage.Main page'), 'titleView'=>trans('ptventa::mainPage.Main page')];
         return view('ptventa::index', compact('view'));
     }
 
     public function devs(){
-        $view = ['titlePage'=>'PTVENTA - Desarrolladores', 'titleView'=>trans('ptventa::devs.Developers and credits')];
+        $view = ['titlePage'=>trans('ptventa::devs.Developers'), 'titleView'=>trans('ptventa::devs.Developers and credits')];
         return view('ptventa::developers.index', compact('view'));
     }
 
     public function info(){
-        $view = ['titlePage'=>'PTVENTA - Información', 'titleView'=>'Acerca de PTVENTA'];
+        $view = ['titlePage'=>trans('ptventa::about.About us'), 'titleView'=>trans('ptventa::about.About us')];
         return view('ptventa::information.index', compact('view'));
     }
 }
--- Modules/PTVENTA/Resources/views/information/index.blade.php
+++ Modules/PTVENTA/Resources/views/information/index.blade.php
@@ -1,7 +1,7 @@
 @extends('ptventa::layouts.master')
 
 @push('breadcrumbs')
-    <li class="breadcrumb-item active">Acerca de:</li>
+    <li class="breadcrumb-item active">{{ trans('ptventa::about.About us')}}</li>
 @endpush
 
 @section('content')
@@ -9,44 +9,41 @@
         <div class="container py-5">
             <h1 class="text-body-emphasis">{{ $view['titleView'] }}</h1>
             <p class="col-lg-8 mx-auto lead">
-                PTVenta nació en el año 2022, como un módulo de SICEFA, su objetivo es administrar la unidad productiva
-                Punto de Venta del centro de formación agroindustrial "La Angostura".
+              {{ trans('ptventa::about.Born in 2022, as a module of SICEFA, its objective is to manage the Point of Sale production unit of the agro-industrial training center "La Angostura".')}}
             </p>
         </div>
     </div>
 
-    <div class="b-example-divider"></div>
-
-    <div class="container px-4 py-5">
+    <div class="container px-4">
         <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
             <div class="col-xl-6">
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="info-box card text-center">
                             <i class="fas fa-map-marked-alt fa-3x mb-3"></i>
-                            <h3>Encuéntranos!</h3>
-                            <p>Centro de Formación Agroindustrial La Angostura<br>Campoalegre, Huila</p>
+                            <h3>{{ trans('ptventa::about.Find us!')}}</h3>
+                            <p>{{ trans('ptventa::about.Agroindustrial Training Center "La Angostura"')}}<br>Campoalegre, Huila</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="info-box card text-center">
                             <i class="far fa-clock fa-3x mb-3"></i>
-                            <h3>Horario de Atención</h3>
-                            <p>Lunes - Viernes<br>08:00AM - 03:00PM</p>
+                            <h3>{{ trans('ptventa::about.Opening Hours')}}</h3>
+                            <p>{{ trans('ptventa::about.Monday - Friday')}}<br>08:00AM - 03:00PM</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="info-box card text-center">
                             <i class="fas fa-phone fa-3x mb-3"></i>
-                            <h3>Llámanos</h3>
-                            <p>Próximamente</p>
+                            <h3>{{ trans('ptventa::about.Contact Us')}}</h3>
+                            <p>{{ trans('ptventa::about.Coming Soon')}}</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="info-box card text-center">
                             <i class="fas fa-envelope fa-3x mb-3"></i>
-                            <h3>Correo Electrónico</h3>
-                            <p>Próximamente</p>
+                            <h3>{{ trans('ptventa::about.Email Address')}}</h3>
+                            <p>{{ trans('ptventa::about.Coming Soon')}}</p>
                         </div>
                     </div>
                 </div>
@@ -58,8 +55,8 @@
                       <i class="fas fa-user-lock fs-4"></i>
                     </div>
                     <div>
-                      <h4 class="fw-semibold mb-0">Seguridad</h4>
-                      <p class="text-body-secondary">Cuenta con un sistema de seguridad óptimo para que la información almacenada sea manejada solo por quienes se desea.</p>
+                      <h4 class="fw-semibold mb-0">{{ trans('ptventa::about.Security')}}</h4>
+                      <p class="text-body-secondary">{{ trans('ptventa::about.It has an optimal security system so that the stored information is handled only by those who want it.')}}</p>
                     </div>
                   </div>
 
@@ -68,8 +65,8 @@
                       <i class="fas fa-tachometer-alt fs-4"></i>
                     </div>
                     <div>
-                      <h4 class="fw-semibold mb-0">Eficiencia</h4>
-                      <p class="text-body-secondary">Los procesos se realizan en el menor tiempo posible, haciendo que el tiempo de respuesta ante una petición sea casi instantáneo.</p>
+                      <h4 class="fw-semibold mb-0">{{ trans('ptventa::about.Efficiently')}}</h4>
+                      <p class="text-body-secondary">{{ trans('ptventa::about.The processes are carried out in the shortest possible time, making the response time to a request almost instantaneous.')}}</p>
                     </div>
                   </div>
 
@@ -78,8 +75,8 @@
                       <i class="fas fa-tachometer-alt fs-4"></i>
                     </div>
                     <div>
-                      <h4 class="fw-semibold mb-0">Diseño</h4>
-                      <p class="text-body-secondary">Cuenta con un diseño elegante y minimalista, agradable para el cliente interno, donde puede hacer uso de este sistema sin complicaciones.</p>
+                      <h4 class="fw-semibold mb-0">{{ trans('ptventa::about.Desing')}}</h4>
+                      <p class="text-body-secondary">{{ trans('ptventa::about.It has an elegant and minimalist design, pleasant for the internal customer, where he can make use of this system without complications.')}}</p>
                     </div>
                   </div>
 
@@ -88,8 +85,8 @@
                       <i class="fas fa-layer-group fs-4"></i>
                     </div>
                     <div>
-                      <h4 class="fw-semibold mb-0">Organizado</h4>
-                      <p class="text-body-secondary">Toda la información al alcance de la mano, organizada de la manera más adecuada.</p>
+                      <h4 class="fw-semibold mb-0">{{ trans('ptventa::about.Organized')}}</h4>
+                      <p class="text-body-secondary">{{ trans('ptventa::about.All the information at your fingertips, organized in the most appropriate way.')}}</p>
                     </div>
                   </div>
                 </div>
