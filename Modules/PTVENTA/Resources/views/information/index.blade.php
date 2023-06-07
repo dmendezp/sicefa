f316ee0eb36f5d532d1527c2a15524d1d32d057c
SrManuel-1 <stevenlievano7575@gmail.com>
2023-06-07 17:41:05 -0500

    PTVENTA - Ajustes finales a la vista acerca de

--- Modules/PTVENTA/Resources/views/information/index.blade.php
+++ Modules/PTVENTA/Resources/views/information/index.blade.php
@@ -1,12 +1,17 @@
 @extends('ptventa::layouts.master')
 
+@push('head')
+    <!-- Estilos de la galería de imágenes -->
+    <link href="{{asset('libs/AOS-2.3.1/dist/aos.css')}}" rel="stylesheet">
+@endpush
+
 @push('breadcrumbs')
     <li class="breadcrumb-item active">{{ trans('ptventa::about.About us')}}</li>
 @endpush
 
 @section('content')
     <div class="p-5 text-center bg-body-tertiary">
-        <div class="container py-5">
+        <div class="container" data-aos="zoom-in">
             <h1 class="text-body-emphasis">{{ $view['titleView'] }}</h1>
             <p class="col-lg-8 mx-auto lead">
               {{ trans('ptventa::about.Born in 2022, as a module of SICEFA, its objective is to manage the Point of Sale production unit of the agro-industrial training center "La Angostura".')}}
@@ -19,28 +24,28 @@
             <div class="col-xl-6">
                 <div class="row">
                     <div class="col-lg-6">
-                        <div class="info-box card text-center">
+                        <div class="info-box card text-center" data-aos="fade-down-right">
                             <i class="fas fa-map-marked-alt fa-3x mb-3"></i>
                             <h3>{{ trans('ptventa::about.Find us!')}}</h3>
                             <p>{{ trans('ptventa::about.Agroindustrial Training Center "La Angostura"')}}<br>Campoalegre, Huila</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <div class="info-box card text-center">
+                        <div class="info-box card text-center" data-aos="fade-down-left">
                             <i class="far fa-clock fa-3x mb-3"></i>
                             <h3>{{ trans('ptventa::about.Opening Hours')}}</h3>
                             <p>{{ trans('ptventa::about.Monday - Friday')}}<br>08:00AM - 03:00PM</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <div class="info-box card text-center">
+                        <div class="info-box card text-center" data-aos="fade-up-right">
                             <i class="fas fa-phone fa-3x mb-3"></i>
                             <h3>{{ trans('ptventa::about.Contact Us')}}</h3>
                             <p>{{ trans('ptventa::about.Coming Soon')}}</p>
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <div class="info-box card text-center">
+                        <div class="info-box card text-center" data-aos="fade-up-left">
                             <i class="fas fa-envelope fa-3x mb-3"></i>
                             <h3>{{ trans('ptventa::about.Email Address')}}</h3>
                             <p>{{ trans('ptventa::about.Coming Soon')}}</p>
@@ -48,7 +53,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col" data-aos="fade-left">
                 <div class="row row-cols-1 row-cols-sm-2 g-4">
                   <div class="col d-flex align-items-start gap-2">
                     <div class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-success bg-gradient rounded-1 p-2">
@@ -91,12 +96,13 @@
                   </div>
                 </div>
               </div>
-
-
-
         </div>
     </div>
 @endsection
 
-@section('scripts')
-@endsection
+@push('scripts')
+    <script src="{{asset('libs/AOS-2.3.1/dist/aos.js')}}"></script>
+    <script>
+        AOS.init();
+    </script>
+@endpush
