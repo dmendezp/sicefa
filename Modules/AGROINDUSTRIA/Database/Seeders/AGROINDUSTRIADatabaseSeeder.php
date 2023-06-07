1733e564f4c1ac878bfb4b2155bf32e9233fdd4d
Jennyferm05 <jennyfermarin05@gmail>
2023-06-07 14:15:36 -0500

    AGROINDUSTRIA - Creacion de modulo AGROINDUSTRIA

---
+++ Modules/AGROINDUSTRIA/Database/Seeders/AGROINDUSTRIADatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\AGROINDUSTRIA\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class AGROINDUSTRIADatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
---
+++ Modules/AGROINDUSTRIA/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('agroindustria::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('agroindustria.name') !!}
+    </p>
+@endsection
