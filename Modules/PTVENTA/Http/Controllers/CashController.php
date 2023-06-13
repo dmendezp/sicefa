d0d757ded3a0313d44cb069da1254a84345dba1e
SrManuel-1 <stevenlievano7575@gmail.com>
2023-06-13 14:27:47 -0500

    PTVENTA - Ajustes a cierre de caja

--- Modules/PTVENTA/Http/Controllers/CashController.php
+++ Modules/PTVENTA/Http/Controllers/CashController.php
@@ -3,16 +3,18 @@
 namespace Modules\PTVENTA\Http\Controllers;
 
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Modules\PTVENTA\Entities\CashCount;
 use Modules\SICA\Entities\Warehouse;
 
 class CashController extends Controller
 {
- /**
+    /**
      * Display a listing of the resource.
      * @return Renderable
      */
@@ -30,7 +32,6 @@ class CashController extends Controller
      */
     public function store(Request $request)
     {
-
         // Verificar si hay una caja abierta
         $openCashCount = CashCount::where('state', 'Abierta')->first();
         if ($openCashCount) {
@@ -40,9 +41,8 @@ class CashController extends Controller
             'initial_balance' => 'required|numeric',
         ]);
 
-        
         $warehouse = Warehouse::where('name','Punto de venta')->first();
-    
+
         $cashCount = new CashCount();
         $cashCount->person_id = Auth::user()->person_id;
         $cashCount->warehouse_id = $warehouse->id;
@@ -53,10 +53,9 @@ class CashController extends Controller
         $cashCount->closing_date = $request->closing_date;
         $cashCount->state = 'Abierta';
         $cashCount->save();
-    
+
         return redirect()->route('ptventa.cash.index')->with('success', ' ');
     }
-    
 
     /**
      * Show the form for closing the cash count.
@@ -72,20 +71,38 @@ class CashController extends Controller
 
     public function close(Request $request)
     {
-        // Validar los datos recibidos
-        $validatedData = $request->validate([
-            'final_balance' => 'required',
-        ]);
-    
-        $cashCount = CashCount::find($request->input('cash_count_id'));
-    
-        $cashCount->final_balance = $validatedData['final_balance'];
-        $cashCount->difference = $cashCount->final_balance - $cashCount->initial_balance;
-        $cashCount->closing_date = Carbon::now();
-        $cashCount->state = 'Cerrada';
-        $cashCount->save();
-    
-        return redirect()->back()->with('success', trans('ptventa::cash.Successfully closed box.'));
+        try {
+            DB::beginTransaction();
+            // Validar los datos recibidos
+            $validatedData = $request->validate([
+                'final_balance' => 'required',
+            ]);
+
+            $cashCount = CashCount::find($request->input('cash_count_id'));
+
+            $cashCount->final_balance = $validatedData['final_balance'];
+            $cashCount->difference = $cashCount->final_balance - $cashCount->initial_balance;
+            $cashCount->closing_date = Carbon::now();
+            $cashCount->state = 'Cerrada';
+            
+            $cashCount->save();
+            
+            CashCount::create([
+                'person_id' => Auth::user()->person_id,
+                'warehouse_id' =>  Warehouse::where('name','Punto de venta')->first()->id,
+                'opening_date' => Carbon::now()->addDay(),
+                'initial_balance' => 0,
+                'final_balance' => null,
+                'difference' => null,
+                'closing_date' => null,
+                'state' => 'Abierta',
+            ]);
+
+            return redirect()->back()->with('success', trans('ptventa::cash.Successfully closed cash.'));
+        } catch (Exception $e) { // Capturar error durante la transacción
+            // Transacción rechazada
+            DB::rollBack(); // Devolver cambios realizados durante la transacción
+            return redirect()->back()->with('error', trans('ptventa::cash.Failed closed cash.'));
+        }
     }
-    
 }
