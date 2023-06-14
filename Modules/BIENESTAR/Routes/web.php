3c1938298af65037c68eb3037df6ce9cbff44036
JuanDR403 <morricito.cerquera@gmail.com>
2023-06-14 14:11:31 -0500

    BIENESTAR - Se crean las rutas de las vistas

--- Modules/BIENESTAR/Routes/web.php
+++ Modules/BIENESTAR/Routes/web.php
@@ -14,3 +14,41 @@
 Route::prefix('bienestar')->group(function() {
     Route::get('/', 'BIENESTARController@index');
 });
+
+
+
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/APEalimentacion', 'BIENESTARController@APEalimentacion')->name('bienestar.APEalimentacion');
+});
+
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/APEinterno', 'BIENESTARController@APEinterno')->name('bienestar.APEinterno');
+});
+
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/APEsena', 'BIENESTARController@APEsena')->name('bienestar.APEsena');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/APEtransporte', 'BIENESTARController@APEtransporte')->name('bienestar.APEtransporte');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/HISeventos', 'BIENESTARController@HISeventos')->name('bienestar.HISeventos');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/home', 'BIENESTARController@home')->name('bienestar.home');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/LIDretorant', 'BIENESTARController@LIDretorant')->name('bienestar.LIDretorant');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/LIDrutas', 'BIENESTARController@LIDrutas')->name('bienestar.LIDrutas');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/SCANrestorant', 'BIENESTARController@SCANrestorant')->name('bienestar.SCANrestorant');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/SCANrutas', 'BIENESTARController@SCANrutas')->name('bienestar.SCANrutas');
+});
+Route::prefix('/bienestar')->group(function() {
+    Route::get('/APEformulario', 'BIENESTARController@APEformulario')->name('bienestar.APEformulario');
+});
