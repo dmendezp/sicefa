b2c76df07e0d8019150820a0bc7e4de81e81212c
BrayanDavidL <brayangarzon474@gmail.com>
2023-06-14 14:43:40 -0500

    BIENESTAR - se agrega la vista APEinterno

---
+++ Modules/BIENESTAR/Resources/views/APEinterno.blade.php
@@ -0,0 +1,100 @@
+@extends('bienestar::layouts.adminlte')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                
+
+            </div>
+
+        </div>
+
+    </div>
+    <div class="row">
+    <div class="col-4">
+        <br>
+      <div class="card">
+        <button>
+  <span>INTERNOS ACTIVOS</span>
+</button>
+
+            </div>
+            <br>
+            <br>
+            <div class="card">
+        <button>
+  <span>INTERNOS EGRESADOS</span>
+</button>
+
+            </div>
+            <br>
+            <br>
+            <div class="card">
+        <button>
+  <span>BLOQUES</span>
+</button>
+
+            </div>
+            <br>
+            <br>
+            <div class="card">
+        <button>
+  <span>...</span>
+</button>
+
+            </div>
+    </div>
+    <div class="col-8">
+      <div class="card-body">
+        <section>
+                         <img src="../bienestarxd/img1/xd.jpg" class="img-fluid" alt="...">
+                         <img src="../bienestarxd/img1/int2.jpg" class="img-fluid" alt="...">
+                         <img src="../bienestarxd/img1/int3.jpg" class="img-fluid" alt="...">
+                     </section>
+
+
+            </div>
+    </div>
+  </div>
+</div>
+<br>
+    <br>
+      <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+    <p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>
+  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+<p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+<p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+<p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+  </div>
+</div>@endsection
