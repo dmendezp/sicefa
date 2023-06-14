6cd0d27d683166291f6ed1adc23f71d7903271a8
Kevinvaldez77 <santiagovaldez1214@gmail.com>
2023-06-14 14:49:31 -0500

    BIENESTAR - Se agrega la vista HISeventos

---
+++ Modules/BIENESTAR/Resources/views/HISeventos.blade.php
@@ -0,0 +1,141 @@
+@extends('bienestar::layouts.adminlte')
+
+@section('content')
+<div class="conta">
+    <div class="content-wrapper1">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          
+          <div class="col-sm-6">
+            <h1>Calendar</h1>
+          </div>
+          <div class="col-sm-6">
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="sticky-top mb-3">
+              <div class="card">
+                <div class="card-header">
+                  <h4 class="card-title">Draggable Events</h4>
+                </div>
+                <div class="card-body">
+                  <!-- the events -->
+                  <div id="external-events">
+                    <div class="external-event bg-success">Lunch</div>
+                    <div class="external-event bg-warning">Go home</div>
+                    <div class="external-event bg-info">Do homework</div>
+                    <div class="external-event bg-primary">Work on UI design</div>
+                    <div class="external-event bg-danger">Sleep tight</div>
+                    <div class="checkbox">
+                      <label for="drop-remove">
+                        <input type="checkbox" id="drop-remove">
+                        remove after drop
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Create Event</h3>
+                </div>
+                <div class="card-body">
+                  <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
+                    <ul class="fc-color-picker" id="color-chooser">
+                      <li><a class="text-primary" href="#"><i class="fas fa-square"></i></a></li>
+                      <li><a class="text-warning" href="#"><i class="fas fa-square"></i></a></li>
+                      <li><a class="text-success" href="#"><i class="fas fa-square"></i></a></li>
+                      <li><a class="text-danger" href="#"><i class="fas fa-square"></i></a></li>
+                      <li><a class="text-muted" href="#"><i class="fas fa-square"></i></a></li>
+                    </ul>
+                  </div>
+                  <!-- /btn-group -->
+                  <div class="input-group">
+                    <input id="new-event" type="text" class="form-control" placeholder="Event Title">
+
+                    <div class="card">
+        <button class="cardo">
+  <span>ADD</span>
+</button>
+
+            </div>
+                    <!-- /btn-group -->
+                  </div>
+                  <!-- /input-group -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-md-9">
+            <div class="card card-primary">
+              <div class="cardis">
+                <!-- THE CALENDAR -->
+                <div id="calendar"></div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+</div>
+<br>
+    <br>
+      <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+    <p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>
+  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+<p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+<p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+    <div class="col">
+      <div class="card-bodyd">
+  <div class="card-details">
+<p class="text-title">Foto Intructor(a)</p>
+    <p class="text-body">Nombre Instructor(a)</p>  </div>
+  <button class="card-button">Mas informacion</button>
+</div>
+    </div>
+  </div>
+</div>
+<!-- Page specific script -->
+
+@endsection
