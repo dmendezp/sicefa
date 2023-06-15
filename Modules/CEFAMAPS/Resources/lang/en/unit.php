922c8ca224f75ed39227ef23f300f13a7c6d6c9a
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-06-15 11:27:20 -0500

    CEFAMAPS - Restauración del módulo

---
+++ Modules/CEFAMAPS/Resources/lang/en/unit.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'Units' => 'Units',
+    'Name' => 'Name',
+    'Person in charge' => 'Person In Charge',
+    'Of The' => 'Of The',
+    'Unit' => 'Unit',
+    'Description' => 'Description',
+    'Add' => 'Agregar',
+    'Icon' => 'Icon',
+    'Farm' => 'Farm',
+    'Number' => 'Number',
+    'Document' => 'Document',
+    'Document notfound' => 'Document number does not exist',
+    'AlertDocumentText' => 'You have not written any Document number',
+    'AlertDocumentTitle' => 'Write your Document number',
+    /* Icon traduction */
+    'Hippo' => 'Hippo',
+    'Otter' => 'Otter',
+    'Dog'=> 'Dog',
+    'Fish'=> 'Fish',
+    'Cow'=> 'Cow',
+    'Shrimp'=> 'Shrimp',
+    'Horse'=> 'Horse',
+    'Frog' => 'Frog',
+    'Dove'=>'Dove',
+    'Cat'=> 'Cat',
+    'Piggy' => 'Piggy',
+    'Lemon' => 'Lemon',
+    'Rice' => 'Rice',
+    'Wheat Building' => 'Wheat Building',
+    'Tree Mango' => 'tree mango',
+    'Coffee' => 'Coffee'
+    
+];
