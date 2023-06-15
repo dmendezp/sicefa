e9eae93c89376d1267655add54331448b09c98a5
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-06-15 11:23:08 -0500

    SICEFA - Eliminación de recursos del módulo de CEFAMAPS

--- Modules/CEFAMAPS/Resources/views/layouts/partials/footer.blade.php
+++
@@ -1,7 +0,0 @@
-  <footer class="main-footer bg-ligth">
-    <strong>Copyright &copy; 2022-2023 <a href="#" style="text-decoration: none">ADSI CODING GROUP</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version Alpha</b> 1.0
-    </div>
-  </footer>
