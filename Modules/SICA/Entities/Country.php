08f5b1a6644dd71997f7a262fe1b6bbd7593f0f3
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-06-16 14:25:11 -0500

    SICA - Verificación de Country

--- Modules/SICA/Entities/Country.php
+++ Modules/SICA/Entities/Country.php
@@ -9,12 +9,18 @@ use Modules\SICA\Entities\Department;
 
 class Country extends Model implements Auditable
 {
-    use SoftDeletes;
-    use \OwenIt\Auditing\Auditable;
-    protected $fillable = [];
-    protected $dates = ['deleted_at'];
-    protected $hidden = ['created_at','updated_at'];
 
+    use \OwenIt\Auditing\Auditable; // Seguimientos de cambios realizados en BD
+
+    use SoftDeletes; // Borrado suave
+
+    protected $fillable = ['name']; // Atributos modificables (asignación masiva)
+
+    protected $dates = ['deleted_at']; // Atributos que deben ser tratados como objetos Carbon
+
+    protected $hidden = ['created_at','updated_at']; // Datos para ocultar en una respuestar array o JSON
+
+    // RELACIONES
     public function departments(){
         return $this->hasMany(Department::class);
     }
