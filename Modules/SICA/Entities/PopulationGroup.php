f697daf94af9a703264c62a5abb3979c4ecb2ff5
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-06-16 15:21:00 -0500

    SICA - Verificación de PopulationGroup

--- Modules/SICA/Entities/PopulationGroup.php
+++ Modules/SICA/Entities/PopulationGroup.php
@@ -14,21 +14,24 @@ class PopulationGroup extends Model implements Auditable
 
     use SoftDeletes; // Borrado suave
 
-    protected $fillable = ['name','description']; // Atributos modificables (asignación masiva)
+    protected $fillable = [ // Atributos modificables (asignación masiva)
+        'name',
+        'description'
+    ];
 
-    protected $dates = ['deleted_at']; // Atributos que deben ser tratados como objetos Carbon (para aprovechar las funciones de formato y manipulación de fecha y hora)
+    protected $dates = ['deleted_at']; // Atributos que deben ser tratados como objetos Carbon
 
-    protected $hidden = [ // Atributos ocultos para no reprensentarlos en las salidas con formato JSON
+    protected $hidden = [ // Datos para ocultar en una respuesta array o JSON
         'created_at',
         'updated_at'
     ];
 
     // MUTADORES Y ACCESORES
-    public function setNameAttribute($value){ // Convierte todos los caracteres en mayúsculas del dato name (MUTADOR)
-        $this->attributes['name'] = mb_strtoupper($value);
-    }
     public function setDescriptionAttribute($value){ // Convierte el primer carácter en mayúscula del dato description (MUTADOR)
         $this->attributes['description'] = ucfirst($value);
+    }
+    public function setNameAttribute($value){ // Convierte todos los caracteres en mayúsculas del dato name (MUTADOR)
+        $this->attributes['name'] = mb_strtoupper($value);
     }
 
     // RELACIONES
