b7519f37322e3fdc0386e2115c8b81311473b1c7
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-06-22 11:59:56 -0500

    SICA - Sincronización de KindOfPurchase

--- Modules/SICA/Entities/KindOfPurchase.php
+++ Modules/SICA/Entities/KindOfPurchase.php
@@ -10,29 +10,28 @@ use OwenIt\Auditing\Contracts\Auditable;
 class KindOfPurchase extends Model implements Auditable
 {
 
-    use \OwenIt\Auditing\Auditable; // Seguimientos de cambios realizados en BD
-
-    use SoftDeletes; // Borrado suave
-
-    use HasFactory; // Generación de datos de prueba
+    use \OwenIt\Auditing\Auditable, // Seguimientos de cambios realizados en BD
+        SoftDeletes, // Borrado suave
+        HasFactory; // Generación de datos de prueba
 
     protected $fillable = [ // Atributos modificables (asignación masiva)
         'name',
         'description'
     ];
 
-    protected $dates = [ // Atributos que deben ser tratados como objetos Carbon (para aprovechar las funciones de formato y manipulación de fecha y hora)
-        'deleted_at',
+    protected $dates = ['deleted_at']; // Atributos que deben ser tratados como objetos Carbon
+
+    protected $hidden = [// Atributos ocultos para no representarlos en las salidas con formato JSON
         'created_at',
         'updated_at'
     ];
 
     // MUTADORES Y ACCESORES
-    public function setNameAttribute($value){ // Convierte el primer carácter en mayúscula y el resto en minúsculas del dato name (MUTADOR)
-        $this->attributes['name'] = ucfirst(strtolower($value));
-    }
     public function setDescriptionAttribute($value){ // Convierte el primer carácter en mayúscula del dato description (MUTADOR)
         $this->attributes['description'] = ucfirst($value);
+    }
+    public function setNameAttribute($value){ // Convierte el primer carácter en mayúscula y el resto en minúsculas del dato name (MUTADOR)
+        $this->attributes['name'] = ucfirst(strtolower($value));
     }
 
     // RELACIONES
