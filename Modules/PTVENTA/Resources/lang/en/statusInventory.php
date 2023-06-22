8e778bab15f54884c16028e2e79f53748a8fcae8
SrManuel-1 <stevenlievano7575@gmail.com>
2023-06-22 10:03:57 -0500

    PTVENTA - Internacionalizacion y diseño de la vista estado de productos

---
+++ Modules/PTVENTA/Resources/lang/en/statusInventory.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    //Breadcrumbs StatusInventory
+	'Inventory' => 'Inventory',
+	'Product Status' => 'Product Status',
+    
+    //Title
+	'All Products' => 'All Products',
+    
+    //Buttons
+	'Registration of Low' => 'Registration of Low',
+    
+    //Tables
+	'Expired' => 'Expired',
+	'To Expired' => 'To Expired',
+    //Head
+	'Amount' => 'Amount',
+	'Products' => 'Products',
+	'Expiration Date' => 'Expiration Date',
+];
--- Modules/PTVENTA/Resources/views/inventory/status.blade.php
+++ Modules/PTVENTA/Resources/views/inventory/status.blade.php
@@ -1,110 +1,101 @@
 @extends('ptventa::layouts.master')
-<style type="text/css">
-    .card{
-  /* Estilos para la card */
-  background-color: #fff;
-  border-radius: 4px;
-  padding: 20px;
 
-}
-
-.tabla-container {
-  max-height: 350px; /* Establece una altura máxima para el contenedor */
-  overflow: auto; /* Habilita las barras de desplazamiento cuando sea necesario */
-}
-
-.tabla-estilo {
-  /* Estilos para las tablas */
-  border-collapse: collapse;
-  width: 100%;
-}
-
-.tabla-estilo th,
-.tabla-estilo td {
-  /* Estilos para las celdas de encabezado y datos */
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-.tabla-estilo th {
-  /* Estilos específicos para las celdas de encabezado */
-  background-color: #f2f2f2;
-}
-</style>
+@push('head')
+    <link href="{{ asset('libs/AOS-2.3.1/dist/aos.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('modules/ptventa/css/table_status.css') }}">
+@endpush
 
 @push('breadcrumbs')
     <li class="breadcrumb-item active">
-        <a href="{{ route('ptventa.inventory.index') }}" class="text-decoration-none">Inventario</a>
+        <a href="{{ route('ptventa.inventory.index') }}" class="text-decoration-none">{{ trans('ptventa::statusInventory.Inventory')}}</a>
     </li>
-    <li class="breadcrumb-item active">Estado de productos</li>
+    <li class="breadcrumb-item active">{{ trans('ptventa::statusInventory.Product Status')}}</li>
 @endpush
 
 @section('content')
-<div class="container">
-    <div class="text-end">
-    <div class="col-auto pe-2">
-            <a href="{{ route('ptventa.inventory.low') }}" class="btn btn-success btn-sm"> Registro de bajas </a>
+    <div class="container" data-aos="fade-down">
+        <div class="d-flex justify-content-around">
+            <div class="col-9">
+                <h6 class="text-center bg-secondary py-2 rounded-2"><strong>{{ trans('ptventa::statusInventory.All Products')}}</strong></h6>
+            </div>
+            <div class="col-3">
+                <a href="{{ route('ptventa.inventory.low') }}" class="btn btn-success"> {{ trans('ptventa::statusInventory.Registration of Low')}} <i class="far fa-hand-point-down"></i> </a>
+            </div>
         </div>
     </div>
-</div>
-    <hr>
-    <h6 class="text-center bg-secondary py-2 rounded-2"><strong>Todos los productos</strong></h6>
- <div class="card border-success mb-3">
-    <div class="card-body">
-      <div class="row">
-          <div class="col-md-6">
-            <h6 class="text-center bg-success py-1 rounded-2"><strong>VENCIDOS</strong></h6>
-            <div class="tabla-container">
-            <table class="tabla-estilo">
-                <thead class="table-secondary">
-                  <th class="text-center">Cantidad</th>
-                  <th class="text-center">Productos</th>
-                  <th class="text-center">Fecha</th>
-                </tr>
-              </thead>
-              <tbody class="table-group-divider">
-                 <div class="tabla-container">
-                @foreach ($productosVencidos as $producto)
-                <tr>
-                  <td>{{ $producto->amount }}</td>
-                  <td>{{ $producto->element->name }}</td>
-                  <td class="text-center">{{ $producto->expiration_date }}</td>
-                </tr>
-                @endforeach
-              </tbody>
-            </thead>
-          </table>
-            </div>  
-          </div>
-          <div class="col-md-6">
-        <h6 class="text-center bg-success py-1 rounded-2"><strong>POR VENCER</strong></h6>
-        <div class="tabla-container">
-          <table class="tabla-estilo">
-            <thead class="table-secondary">
-              <th class="text-center">Cantidad</th>
-              <th class="text-center">Productos</th>
-              <th class="text-center">Fecha</th>
-            </tr>
-          </thead>
-          <tbody class="table-group-divider">
-            @foreach ($productosPorVencer as $producto)
-            <tr>
-              <td>{{ $producto->amount }}</td>
-              <td>{{ $producto->element->name }}</td>
-              <td class="text-center">{{ $producto->expiration_date }}</p>
-            </tr>
-            @endforeach
-          </tbody>
-        </thead>
-      </table>
+    <hr data-aos="fade-left" data-aos-duration="3000">
+    <div class="card border-success mb-3" data-aos="fade-up">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <h6 class="text-center bg-danger py-1 rounded-2"><strong>{{ trans('ptventa::statusInventory.Expired')}}</strong></h6>
+                    <div class="tabla-container">
+                        <table class="table table-hover table-bordered" id="tableExpiredProducts">
+                            <thead class="table-secondary">
+                                <th class="text-center">{{ trans('ptventa::statusInventory.Amount')}}</th>
+                                <th>{{ trans('ptventa::statusInventory.Products')}}</th>
+                                <th class="text-center">{{ trans('ptventa::statusInventory.Expiration Date')}}</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                <div class="tabla-container">
+                                    @foreach ($productosVencidos as $producto)
+                                        <tr>
+                                            <td class="text-center">{{ $producto->amount }}</td>
+                                            <td>{{ $producto->element->name }}</td>
+                                            <td class="text-center">{{ $producto->expiration_date }}</td>
+                                        </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h6 class="text-center bg-warning py-1 rounded-2"><strong>{{ trans('ptventa::statusInventory.To Expired')}}</strong></h6>
+                    <div class="tabla-container">
+                        <table class="table table-hover table-bordered" id="tableProductsToExpire">
+                            <thead class="table-secondary">
+                                <th class="text-center">{{ trans('ptventa::statusInventory.Amount')}}</th>
+                                <th>{{ trans('ptventa::statusInventory.Products')}}</th>
+                                <th class="text-center">{{ trans('ptventa::statusInventory.Expiration Date')}}</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                <div class="tabla-container">
+                                    @foreach ($productosPorVencer as $producto)
+                                        <tr>
+                                            <td class="text-center">{{ $producto->amount }}</td>
+                                            <td>{{ $producto->element->name }}</td>
+                                            <td class="text-center">{{ $producto->expiration_date }}</p>
+                                        </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-</div>
-</div>
-
-
 @endsection
 
 @include('ptventa::layouts.partials.plugins.datatables')
 
+@push('scripts')
+    <script src="{{ asset('libs/AOS-2.3.1/dist/aos.js') }}"></script>
+    <script>
+        AOS.init();
+    </script>
+    <script>
+        // Permite la aplicacion de datatables y la vez la traduccion de las tablas
+        $(document).ready(function() {
+            /* Initialización of Datatables CashCount */
+            $('#tableExpiredProducts').DataTable({
+                language: language_datatables, // Agregar traducción a español
+            });
+            /* Initialización of Datatables CashCountAll */
+            $('#tableProductsToExpire').DataTable({
+                language: language_datatables, // Agregar traducción a español
+            });
+        });
+    </script>
+@endpush
