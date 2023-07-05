094b5aefe1a001cb877b1e7712c307a00cc601dd
GabrielGG024 <gtgabriel78@gmail.com>
2023-07-05 10:55:21 -0500

    RADIOCEFA-Cambios al seeder radicefa

--- Modules/RADIOCEFA/Database/Seeders/RADIOCEFADatabaseSeeder.php
+++ Modules/RADIOCEFA/Database/Seeders/RADIOCEFADatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Modules\RADIOCEFA\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\user;
+use Modules\SICA\Entities\Person;
+
 
 class RADIOCEFADatabaseSeeder extends Seeder
 {
@@ -14,8 +17,17 @@ class RADIOCEFADatabaseSeeder extends Seeder
      */
     public function run()
 {
+
+    $person = Person::firstOrCreate(['document_number'=> 1125276647], [
+        'document_type' => 'Tarjeta de identidad',
+        'first_name' => 'MARIA DEL CARMEN',
+        'first_last_name' => 'ROSELL',
+        'second_last_name' => 'PEREZ'
+    ]);
+
     $usuario = new User();
-    $usuario->name = 'Maria Rosell';
+    $usuario->nickname = 'Maria Rosell';
+    $usuario->person_id = $person->id;
     $usuario->email = 'MariaRosell@example.com';
     $usuario->password = bcrypt('contraseña');
     $usuario->save();
