76b13e3f67a1a75d79b73ec186a081cd62f8ef9a
GabrielGG024 <gtgabriel78@gmail.com>
2023-07-05 11:36:12 -0500

    Revert "RADIOCEFA-Cambios al seeder radicefa"
    This reverts commit 094b5aefe1a001cb877b1e7712c307a00cc601dd.

--- Modules/RADIOCEFA/Database/Seeders/RADIOCEFADatabaseSeeder.php
+++ Modules/RADIOCEFA/Database/Seeders/RADIOCEFADatabaseSeeder.php
@@ -4,9 +4,6 @@ namespace Modules\RADIOCEFA\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\user;
-use Modules\SICA\Entities\Person;
-
 
 class RADIOCEFADatabaseSeeder extends Seeder
 {
@@ -17,17 +14,8 @@ class RADIOCEFADatabaseSeeder extends Seeder
      */
     public function run()
 {
-
-    $person = Person::firstOrCreate(['document_number'=> 1125276647], [
-        'document_type' => 'Tarjeta de identidad',
-        'first_name' => 'MARIA DEL CARMEN',
-        'first_last_name' => 'ROSELL',
-        'second_last_name' => 'PEREZ'
-    ]);
-
     $usuario = new User();
-    $usuario->nickname = 'Maria Rosell';
-    $usuario->person_id = $person->id;
+    $usuario->name = 'Maria Rosell';
     $usuario->email = 'MariaRosell@example.com';
     $usuario->password = bcrypt('contraseña');
     $usuario->save();
