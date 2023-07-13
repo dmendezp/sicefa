e2452ce797defbefd8c05055a77ca241e68b64b8
SrManuel-1 <stevenlievano7575@gmail.com>
2023-07-12 19:58:22 -0500

    SIGAC - Implementacion del plugin Fullcalendar-6.1.8

--- Modules/SIGAC/Resources/views/attendance/consult.blade.php
+++ Modules/SIGAC/Resources/views/attendance/consult.blade.php
@@ -55,9 +55,23 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
-
+                    <div id='calendar'></div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+
+    <script src='{{ asset('libs/fullcalendar-6.1.8/dist/index.global.min.js') }}'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth'
+            });
+            calendar.render();
+        });
+    </script>
 @endsection
