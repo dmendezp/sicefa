db720d5f75663d02e49d203eedd5c46b02ac5b23
JuanDR403 <morricito.cerquera@gmail.com>
2023-07-25 13:44:04 -0500

    BIENESTAR - edicion de la vista APEinterno en el que se acomodan botones y agrega un crud temporal

--- Modules/BIENESTAR/Resources/views/APEinterno.blade.php
+++ Modules/BIENESTAR/Resources/views/APEinterno.blade.php
@@ -2,99 +2,84 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
 
-            </div>
 
-        </div>
-
+      </div>
     </div>
-    <div class="row">
-    <div class="col-4">
-        <br>
+  </div>
+
+  <div class="row">
+    <div class="col-3">
+
+      <br>
       <div class="card">
         <button>
-  <span>INTERNOS ACTIVOS</span>
-</button>
-
-            </div>
-            <br>
-            <br>
-            <div class="card">
-        <button>
-  <span>INTERNOS EGRESADOS</span>
-</button>
-
-            </div>
-            <br>
-            <br>
-            <div class="card">
-        <button>
-  <span>BLOQUES</span>
-</button>
-
-            </div>
-            <br>
-            <br>
-            <div class="card">
-        <button>
-  <span>...</span>
-</button>
-
-            </div>
+          <span>INTERNOS ACTIVOS</span>
+        </button>
+      </div>
+      <br>
+      <br>
     </div>
-    <div class="col-8">
-      <div class="card-body">
-        <section>
-                         <img src="../bienestarxd/img1/xd.jpg" class="img-fluid" alt="...">
-                         <img src="../bienestarxd/img1/int2.jpg" class="img-fluid" alt="...">
-                         <img src="../bienestarxd/img1/int3.jpg" class="img-fluid" alt="...">
-                     </section>
-
-
-            </div>
-    </div>
-  </div>
-</div>
-<br>
     <br>
-      <div class="container text-center">
-  <div class="row">
-    <div class="col">
-      <div class="card-bodyd">
-  <div class="card-details">
-    <p class="text-title">Foto Intructor(a)</p>
-    <p class="text-body">Nombre Instructor(a)</p>
+    <div class="col-3">
+      <div class="card-body">
+        <div class="card">
+          <button>
+            <span>INTERNOS EGRESADOS</span>
+          </button>
+        </div>
+        <br>
+        <br>
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="col-3">
+      <div class="card-body">
+        <div class="card">
+          <button>
+            <span>BLOQUES</span>
+          </button>
+        </div>
+        <br>
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="container text-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-body">
+            <table id="miDataTable" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Aprendiz</th>
+                  <th>Programa</th>
+                  <th>Ficha</th>
+                  <th>Apoyo Alimentacion</th>
+                  <th>Apoyo Transporte</th>
+                  <th>Apoyo Internado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Juancho</td>
+                  <td>Adso</td>
+                  <td>2500871</td>
+                  <td>Recibe 00%</td>
+                  <td>Recibe 100%</td>
+                  <td>no recibe</td>
+                </tr>
+                <!-- Agrega más filas con los datos que deseas mostrar -->
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <button class="card-button">Mas informacion</button>
 </div>
-    </div>
-    <div class="col">
-      <div class="card-bodyd">
-  <div class="card-details">
-<p class="text-title">Foto Intructor(a)</p>
-    <p class="text-body">Nombre Instructor(a)</p>  </div>
-  <button class="card-button">Mas informacion</button>
-</div>
-    </div>
-    <div class="col">
-      <div class="card-bodyd">
-  <div class="card-details">
-<p class="text-title">Foto Intructor(a)</p>
-    <p class="text-body">Nombre Instructor(a)</p>  </div>
-  <button class="card-button">Mas informacion</button>
-</div>
-    </div>
-    <div class="col">
-      <div class="card-bodyd">
-  <div class="card-details">
-<p class="text-title">Foto Intructor(a)</p>
-    <p class="text-body">Nombre Instructor(a)</p>  </div>
-  <button class="card-button">Mas informacion</button>
-</div>
-    </div>
-  </div>
-</div>@endsection
+@endsection
