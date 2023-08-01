311a6f80abccb07db94389dbfb09df0ee0bbbe0c
KarenM <dayanamurcia370@gmail.com>
2023-08-01 18:11:07 -0500

    update with Fabrica4

--- Modules/SICA/Entities/Warehouse.php
+++ Modules/SICA/Entities/Warehouse.php
@@ -5,21 +5,9 @@ namespace Modules\SICA\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
-use Modules\GANADERIA\Entities\Productive_proces;
 
 class Warehouse extends Model implements Auditable
 {
-<<<<<<< HEAD
-    use SoftDeletes;
-    use \OwenIt\Auditing\Auditable;
-    protected $dates = ['deleted_at'];
-    protected $hidden = ['created_at','updated_at'];
-    protected $fillable = ['code','star_date','end_date','program-id','deschooling'];
-
-    public function productive_proces() {
-        return $this->hasMany(Productive_proces::class);
-    }
-=======
 
     use \OwenIt\Auditing\Auditable, // Seguimientos de cambios realizados en BD
         SoftDeletes; // Borrado suave
@@ -49,15 +37,8 @@ class Warehouse extends Model implements Auditable
     public function app(){ // Accede a la información de la aplicación al que pertenece
         return $this->belongsTo(App::class);
     }
-    public function inventories(){ // Accede a todos los registros de inventarios que le pertenecen a esta bodega
-        return $this->hasMany(Inventory::class);
-    }
-    public function productive_units(){ // Accede a una o varias unidades productivas asociadas a él (Relación muchos a muchos)
-        return $this->belongsToMany(ProductiveUnit::class);
-    }
-    public function warehouse_movements(){ // Accede a todos los movimientos de bodega que pertenecen a esta bodega
-        return $this->hasMany(WarehouseMovement::class);
+    public function productive_unit_warehouses(){ // Accede a todos los registros de unidad productiva y bodega que pertenecen a esta bodega
+        return $this->hasMany(ProductiveUnitWarehouse::class);
     }
 
->>>>>>> 66f4c04502d915b8977a7de7506bf87ecf0b5caa
 }
