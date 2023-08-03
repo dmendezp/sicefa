2024728612ef171d4ce86a2ff431f09455b89623
SrManuel-1 <stevenlievano7575@gmail.com>
2023-08-03 09:39:03 -0500

    PTVENTA - Internacionalizacion de la vista registro de venta

--- Modules/PTVENTA/Resources/lang/en/sales.php
+++ Modules/PTVENTA/Resources/lang/en/sales.php
@@ -10,9 +10,10 @@ return [
 	'Sales'    => 'Sales',
 	'Today'    => 'Today',
 	'Register' => 'Resgister',
-
-    //Section Card
+  
+	//Section Card
     //Table Titles
+	//Index Sales
     '1T1' => '#',
     '1T2' => 'Voucher',
     '1T3' => 'Client',
@@ -23,12 +24,50 @@ return [
 
     //Section BTNS
     'Btn1'   => 'Register Sale',
-    'Btn2'   => 'Expired / To be expired',
-    'Btn3'   => 'Add Product',
-    'Btn4'   => 'Register Entry',
+    'Btn2'   => 'Register',
+    'Btn3'   => 'Cancel',
     
     //Section optional
     'TextOp1'   => 'No Sales Registered.',
     'TextOp2'   => 'There is no open cash box.',
-    
+	
+	//Section Form
+    'TextForm1'   => 'Product:',
+    'SelectForm1' => '-- Select product --',
+    'TextForm2'   => 'Stocks:',
+    'TextForm3'   => 'Price:',
+    'TextForm4'   => 'Amount:',
+    'TextForm5'   => 'Subtotal:',
+	
+	//Section Card
+    //Table Titles
+	//Register Sales
+    '2T1'   => '#',
+    '2T2'   => 'Product',
+    '2T3'   => 'Amount',
+    '2T4'   => 'Value',
+    '2T5'   => 'Total',
+    '2T6'   => 'Actions',
+	
+	//Tootips
+    'Tooltip1' => 'Update Product',
+    'Tooltip2' => 'Delete Product',
+	
+	//Section Sales Data
+	//Card
+    'TitleCard' => 'Sale',
+    'TextCard' => 'Identification:',
+    'TextCard1' => 'Name:',
+
+	//Section Modal
+	'TitleModal' => 'Register Client',
+	'AlertModal' => 'The person consulted is not registered.',
+	'PlaceholderModal1' => '-- Select --',
+	'PlaceholderModal2' => 'Number',
+	'TextModal1' => 'Identification',
+	'TextModal2' => 'First names',
+	'PlaceholderModal3' => 'First and second name',
+	'TextModal3' => 'Last names',
+	'PlaceholderModal4' => 'First last name',
+	'PlaceholderModal5' => 'Second last name',
 ];
--- Modules/PTVENTA/Resources/lang/es/sales.php
+++ Modules/PTVENTA/Resources/lang/es/sales.php
@@ -4,14 +4,16 @@ return [
     //Origin Controller SaleController
 	'titlePage' => 'Ventas',
 	'titleView' => 'Ventas realizadas hoy',
+	'titleView2'    => 'Registro de Ventas',
 
     //Breadcrumbs
 	'Sales'    => 'Ventas',
 	'Today'    => 'Hoy',
 	'Register' => 'Registro',
 
-    //Section Card
+	//Section Card
     //Table Titles
+	//Index Sales
     '1T1' => 'N°',
     '1T2' => 'Comprobante',
     '1T3' => 'Cliente',
@@ -22,9 +24,50 @@ return [
     
     //Section BTNS
     'Btn1' => 'Registrar Venta',
+    'Btn2' => 'Registrar',
+    'Btn3' => 'Cancelar',
     
-    //Section option
-    'TextOp1' => 'No hay ventas registradas.',
-    'TextOp2' => 'No existe ninguna caja abierta.',
+	//Section option
+	'TextOp1' => 'No hay ventas registradas.',
+	'TextOp2' => 'No existe ninguna caja abierta.',
 
+	//Section Form
+	'TextForm1'   => 'Producto:',
+	'SelectForm1' => '-- Seleccionar producto --',
+	'TextForm2'   => 'Existencias:',
+	'TextForm3'   => 'Precio:',
+	'TextForm4'   => 'Cantidad:',
+	'TextForm5'   => 'Subtotal:',
+	
+	//Section Card
+	//Table Titles
+	//Register Sales
+	'2T1'   => 'Nª',
+	'2T2'   => 'Producto',
+	'2T3'   => 'Cantidad',
+	'2T4'   => 'Valor',
+	'2T5'   => 'Total',
+	'2T6'   => 'Acciones',
+	
+	//Tooltip
+	'Tooltip1' => 'Actualizar Producto',
+	'Tooltip2' => 'Eliminar Producto',
+	
+	//Section Sales Data
+	//Card
+	'TitleCard' => 'Venta',
+	'TextCard' => 'Identificación:',
+	'TextCard1' => 'Nombre:',
+	
+	//Section Modal
+	'TitleModal' => 'Registro de Cliente',
+	'AlertModal' => 'La persona consultada no se encuentra registrado.',
+	'PlaceholderModal1' => '-- Seleccionar --',
+	'PlaceholderModal2' => 'Número',
+	'TextModal1' => 'Identificación',
+	'TextModal2' => 'Nombres',
+	'PlaceholderModal3' => 'Primer y segundo nombre',
+	'TextModal3' => 'Apellidos',
+	'PlaceholderModal4' => 'Primer apellido',
+	'PlaceholderModal5' => 'Segundo apellido',
 ];
