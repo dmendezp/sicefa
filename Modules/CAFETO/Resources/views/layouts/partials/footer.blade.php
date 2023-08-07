3cc1ec02ff30295c8fcd8196c492b908192a7d5f
SrManuel-1 <stevenlievano7575@gmail.com>
2023-08-06 21:06:49 -0500

    CAFETO - Rediseño del footer

--- Modules/CAFETO/Resources/views/layouts/partials/footer.blade.php
+++ Modules/CAFETO/Resources/views/layouts/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="main-footer footer-background-cafeto text-dark">
   <!-- To the right -->
   <div class="float-right d-none d-sm-inline">
-    <b>Version Alpha</b> 1.0 
+    <b>Version Alpha</b> 0.1 
   </div>
   <!-- Default to the left -->
-  <strong>Copyright &copy; 2023 <a href="{{ route('cefa.welcome') }}" style="text-decoration: none">Centro de Fomación Agroindustrial "La Angostura"</a>.</strong> Todos los derechos reservados.
+  <strong>Copyright &copy; 2023 <a href="{{ route('cefa.welcome') }}" style="text-decoration: none; color:white">Fábrica de Software</a>.</strong> Todos los derechos reservados.
 </footer>
 
