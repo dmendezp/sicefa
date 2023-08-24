c7cce008f15b7532b960aa94138b6ac1e6ff3a2a
Jennyferm05 <jennyfermarin05@gmail>
2023-08-23 20:34:45 -0500

    Agroindustria -  Controlador, ruta y vista de formulación

---
+++ Modules/AGROINDUSTRIA/Resources/views/instructor/formulations/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Listado de Formulaciones</h1>
+
+    <ul>
+
+     <li>Formulaciones</li>
+
+    </ul>
+@endsection
--- Modules/AGROINDUSTRIA/Routes/web.php
+++ Modules/AGROINDUSTRIA/Routes/web.php
@@ -6,6 +6,8 @@ use Modules\AGROINDUSTRIA\Http\Controllers\instructor\UnitController;
 
 use Modules\AGROINDUSTRIA\Http\Controllers\Intern\InventoryController;
 
+use Modules\AGROINDUSTRIA\Http\Controllers\Instructor\FormulationController;
+
 Route::prefix('agroindustria')->group(function() {
     Route::get('/index', 'AGROINDUSTRIAController@index')->name('agroindustria.index');
 
@@ -13,7 +15,7 @@ Route::prefix('agroindustria')->group(function() {
     Route::prefix('instructor')->group(function (){
         Route::get('/index', [UnitController::class ,'index'])->name('agroindustria.instructor.index');
         Route::get('/unidd', [UnitController::class, 'unidd'])->name('agroindustria.instructor.unidd');
-        Route::get('/solicitud', [RequestController::class, 'solicitud'])->name('agroindustria.instructor.solicitud');  
+        Route::get('/solicitud', [RequestController::class, 'solicitud'])->name('agroindustria.instructor.solicitud');
         Route::post('/enviarsolicitud', [RequestController::class, 'enviarsolicitud'])->name('agroindustria.instructor.enviarsolicitud');
     });
 
@@ -22,5 +24,8 @@ Route::prefix('agroindustria')->group(function() {
         Route::get('/index', [InventoryController::class ,'index'])->name('agroindustria.intern.index');
         Route::get('/invb', [InventoryController::class ,'invb'])->name('agroindustria.intern.invb');
     });
+
+    Route::get('/formulations', [FormulationController::class, 'index']);
+    Route::get('/formulations/create', [FormulationController::class, 'create']);
 
 });
