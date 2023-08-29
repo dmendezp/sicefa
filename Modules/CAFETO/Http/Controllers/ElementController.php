2fcbe9d6edd2d641862e7dfd9d6b1abb10285607
Anyi <angyrojas835@gmail.com>
2023-08-28 20:41:54 -0500

    CAFETO - Agregar producto

--- Modules/CAFETO/Http/Controllers/ElementController.php
+++ Modules/CAFETO/Http/Controllers/ElementController.php
@@ -4,6 +4,7 @@ namespace Modules\CAFETO\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Routing\Controller;
 use Modules\SICA\Entities\Element;
 use Modules\SICA\Entities\App;
@@ -11,7 +12,7 @@ use Modules\SICA\Entities\Category;
 use Modules\SICA\Entities\MeasurementUnit;
 use Modules\SICA\Entities\KindOfPurchase;
 use PhpParser\Node\Stmt\Return_;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 
 class ElementController extends Controller
 {
@@ -105,63 +106,48 @@ class ElementController extends Controller
         return view('cafeto::element.create', compact('view', 'measurement_units', 'categories', 'kind_of_purchase', 'apps'));
     }
 
-    public function store(Request $request, Element $element){
+    public function store(Request $request){
+        $request->merge(['price' => revertPriceFormat(e($request->input('price')))]); // Limpiar el valor de price
         $rules = [
-            'image' => 'required',
-            'name' => 'required',
+            'name' => 'required|unique:elements',
             'measurement_unit_id' => 'required',
-            'description' => 'required',
             'kind_of_purchase_id' => 'required',
             'category_id' => 'required',
             'price' => 'required',
-            'UNSPSC_code' => 'required',
+            'UNSPSC_code' => 'nullable|integer|unique:elements'
         ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
 
-        $messages = [
-            'image.required' => 'La imagen es obligatoria',
-            'name.required' => 'El nombre del producto es obligatorio',
-            'measurement_unit_id.required' => 'La unidad de medida es obligatoria',
-            'description.required.required'=> 'La descripcion del producto es obligatoria',
-            'kind_of_purchase_id.required' => 'El tipo de compra es obligatoria',
-            'category_id.required' => 'Lacategoria es obligatoria',
-            'price.required' => 'El precio del producto es obligatorio',
-            'UNSPSC_code.required' => 'El codigo del producto es obligatorio',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
-        if ($validator->fails()):
-            return back()->withInput()->withErrors($validator)->with('message-validator','Se ha producido un error. Por favor, verifica el formulario.')->with('typealert','danger');
-        else:
-            $element = new Element();
-
-            if($request->hasFile('image')){ // Verificar si se ha subido una nueva imagen
-                //Obtener la imagen enviada por el usuario
-                $image = $request->file('image');
-
-                //Guardar la nueva imagen en el sistema de archivos
-                $extension =  pathinfo($image->getClientOriginalName(), PATHINFO_EXTENSION); // Capturar la extensión de la nueva imagen
-                $name_image =  $element->slug . '.' . $extension; // Generar el nombre por defecto de la nueva imagen
-                $image->move(public_path('modules/sica/images/elements/'), $name_image); // Guarda la imagen dentro de public
-
-                //Actualizar la ruta de la imagen en la base de datos
-                $element->image = 'modules/sica/images/elements/' . $name_image; // Generar ruta relativa de la nueva imagen
-                $element->name = e($request->input('name'));
-                $element->measurement_unit_id = e($request->input('measurement_unit_id'));
-                $element->description = e($request->input('description'));
-                $element->kind_of_purchase_id = e($request->input('kind_of_purchase_id'));
-                $element->category_id = e($request->input('category_id'));
-                $element->price = e($request->input('price'));
-                $element->UNSPSC_code = e($request->input('UNSPSC_code'));
-                $element->slug = e($request->input('name'));
-
-                if ($element->save()){
-                $message_cafeto = "Elemento agregado exitosamente";
-                $message_cafeto_type = 'success';
-                }else{
-                $message_cafeto = "Se ha producido un error en el momento de agregar el elemento";
-                $message_cafeto_type = 'error';
-                }
-                return redirect(route('cafeto.element.index'))->with('message_cafeto',$message_cafeto)->with('message_cafeto_type',$message_cafeto_type);
-            }
-       endif;
+        // Registro del elemento
+        $element = new Element();
+        $element->name = e($request->input('name'));
+        if($request->hasFile('image')){ // Verificar si se ha subido una nueva imagen
+            //Obtener la imagen enviada por el usuario
+            $image = $request->file('image');
+            //Guardar la nueva imagen en el sistema de archivos
+            $extension =  pathinfo($image->getClientOriginalName(), PATHINFO_EXTENSION); // Capturar la extensión de la nueva imagen
+            $name_image =  $element->slug . '.' . $extension; // Generar el nombre por defecto de la nueva imagen
+            $image->move(public_path('modules/sica/images/elements/'), $name_image); // Guarda la imagen dentro de public
+            //Actualizar la ruta de la imagen en la base de datos
+            $element->image = 'modules/sica/images/elements/' . $name_image; // Generar ruta relativa de la nueva imagen
+        }
+        $element->measurement_unit_id = e($request->input('measurement_unit_id'));
+        $element->description = e($request->input('description'));
+        $element->kind_of_purchase_id = e($request->input('kind_of_purchase_id'));
+        $element->category_id = e($request->input('category_id'));
+        $element->price = e($request->input('price'));
+        $UNSPSC_code = e($request->input('UNSPSC_code'));
+        $element->UNSPSC_code = !empty($UNSPSC_code) ? $UNSPSC_code : null;
+        if ($element->save()){
+            $message_cafeto = "Elemento agregado exitosamente";
+            $message_cafeto_type = 'success';
+        }else{
+            $message_cafeto = "Se ha producido un error en el momento de agregar el elemento";
+            $message_cafeto_type = 'error';
+        }
+        return redirect(route('cafeto.'.getRoleRouteName(Route::currentRouteName()).'.element.index'))->with('message_cafeto', $message_cafeto)->with('message_cafeto_type', $message_cafeto_type);
     }
 }
--- Modules/CAFETO/Resources/views/element/create.blade.php
+++ Modules/CAFETO/Resources/views/element/create.blade.php
@@ -2,181 +2,189 @@
 
 @push('breadcrumbs')
     <li class="breadcrumb-item active">
-        <a href="{{ route('cafeto.element.index') }}" class="text-decoration-none">Productos</a>
+        <a href="{{ route('cafeto.element.index') }}" class="text-decoration-none">Producto</a>
     </li>
-    <li class="breadcrumb-item active">Agregar Producto</li>
+    <li class="breadcrumb-item active">Registrar Producto</li>
 @endpush
 
 @section('content')
-        {!! Form::open(['route'=>'cafeto.element.store', 'method'=>'POST', 'id'=>'form-config']) !!} 
-            @csrf
-                <div class="card card-success card-outline col-10 mx-auto">
-
-                    <div class="card-body pb-0">
-                        <div class="row">
-
-                            <div class="col-4">
-                                <div class="card card-success border-success">
-                                    <div class="card-header text-center h5 py-1">
-                                        Imagen
-                                    </div>
-                                    <div class="card-body mx-auto">
-                                        @if (isset($element))
-                                        <img src="@if ($element->image && file_exists(public_path($element->image))) {{ asset($element->image) }} @else {{ asset('modules/sica/images/sinImagen.png') }} @endif"
-                                            id="imagenSeleccionada" class="img-fluid img-thumbnail"
-                                            style="max-height: 200px; max-width:300px float: left;">
-                                        @else
-                                            <img src="" id="imagenSeleccionada" class="img-fluid img-thumbnail"
-                                                style="max-height: 200px; max-width:300px float: left;">
-                                        @endif
-                                    </div>
-                                </div>
+    {!! Form::open(['route'=>'cafeto.element.store', 'method'=>'POST', 'id'=>'form-element' , 'enctype'=>'multipart/form-data']) !!}
+        @csrf
+        <div class="card card-success card-outline mx-auto mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-success border-success">
+                            <div class="card-header text-center h5 py-1">
+                                Imagen
                             </div>
-
-                            <div class="col-md-4">
-                                <div class="mb-3">
-                                    <label for="formFile" class="form-label">Seleccionar imagen</label>
+                            <div class="card-body text-center">
+                                <img src="{{ asset('modules/sica/images/sinImagen.png') }}" id="imagenSeleccionada" class="img-fluid img-thumbnail" style="max-height: 200px; max-width:300px float: left;">
+                                <hr>
+                                <div class="my-0 text-left">
+                                    <label for="formFile" class="form-label">Seleccione</label>
                                     <input type="file" name="image" id="image" class="form-control">
                                 </div>
-
-                                {!! Form::label('name', 'Nombre:', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="far fa-keyboard"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::text('name', isset($element) ? $element->name : null, [
-                                        'class' => 'form-control',
-                                        'required',
-                                        'placeholder' => 'Nombre',
-                                    ]) !!}
-                                </div>
-
-                                {!! Form::label('measurement_unit', 'Medida:', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="fas fa-list"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::select(
-                                        'measurement_unit_id',
-                                        $measurement_units,
-                                        isset($element) ? $element->measurement_unit_id : null,
-                                        [
-                                            'placeholder' => '-- Seleccione --',
-                                            'class' => 'form-control',
-                                            'required',
-                                        ],
-                                    ) !!}
-                                </div>
-
-                                {!! Form::label('description', 'Descripción:', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="far fa-keyboard"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::text('description', isset($element) ? $element->description : null, [
-                                        'class' => 'form-control',
-                                        'required',
-                                        'placeholder' => 'Descripción',
-                                    ]) !!}
-                                </div>
                             </div>
-
-                            <div class="col-md-4">
-                                {!! Form::label('price', 'Precio:', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="far fa-keyboard"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::text('price', isset($element) ? $element->price : null, [
-                                        'class' => 'form-control',
-                                        'required',
-                                        'placeholder' => 'precio',
-                                    ]) !!}
-                                </div>
-
-                                {!! Form::label('category_id', 'Categoria:', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="fas fa-list"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::select('category_id', $categories, isset($element) ? $element->category_id : null, [
-                                        'class' => 'form-control',
-                                        'required',
-                                        'placeholder' => 'Categoria',
-                                    ]) !!}
-                                </div>
-
-                                {!! Form::label('UNSPSC_code', 'Codigo', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="far fa-keyboard"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::text('UNSPSC_code', isset($element) ? $element->UNSPSC_code : null, [
-                                        'class' => 'form-control',
-                                        'placeholder' => 'Codigo',
-                                    ]) !!}
-                                </div>
-
-                                {!! Form::label('kind_of_purchase', 'Tipo de Compra:', ['class' => 'mt-3']) !!}
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="fas fa-list"></i>
-                                        </span>
-                                    </div>
-                                    {!! Form::select(
-                                        'kind_of_purchase_id',
-                                        $kind_of_purchase,
-                                        isset($element) ? $element->kind_of_purchase_id : null,
-                                        [
-                                            'class' => 'form-control',
-                                            'required',
-                                            'placeholder' => 'Tipo de Compra',
-                                        ],
-                                    ) !!}
-                                </div>
-                            </div>
-
-
                         </div>
                     </div>
-                    <br><br>
+                    <div class="col-md-4">
+                        {!! Form::label('name', 'Nombre', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="far fa-keyboard"></i>
+                                </span>
+                            </div>
+                            {!! Form::text('name', null, [
+                                'class' => 'form-control',
+                                'required'
+                            ]) !!}
+                        </div>
+                        @error('name')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
 
-                    <div class="card-footer bg-white text-right">
-                        <a href="{{ route('cafeto.element.index') }}" class="btn btn-sm btn-light mr-2">
-                            <b>Cancelar</b>
-                        </a>
-                        <button type="submit" class="btn btn-sm btn-success">
-                            <b>Guardar</b>
-                        </button>
+                        {!! Form::label('measurement_unit', 'Unidad de Medida', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-list"></i>
+                                </span>
+                            </div>
+                            <select name="measurement_unit_id" class="form-select" required>
+                                <option value="">Seleccione...</option>
+                                @foreach ($measurement_units as $mu)
+                                    <option value="{{ $mu->id }}" {{ old('measurement_unit_id') == $mu->id ? 'selected' : '' }}>{{ $mu->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error('measurement_unit_id')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
+
+                        {!! Form::label('description', 'Descripciòn', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="far fa-keyboard"></i>
+                                </span>
+                            </div>
+                            {!! Form::textarea('description', null, ['class' => 'form-control', 'rows' => '5']) !!}
+                        </div>
+                        @error('description')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="col-md-4">
+                        {!! Form::label('price', 'Precio', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="far fa-keyboard"></i>
+                                </span>
+                            </div>
+                            {!! Form::text('price', null, [
+                                'class' => 'form-control price-format',
+                                'id' => 'price',
+                                'required'
+                            ]) !!}
+                        </div>
+                        @error('price')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
+
+                        {!! Form::label('category_id', 'Categoria', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-list"></i>
+                                </span>
+                            </div>
+                            <select name="category_id" class="form-select" required>
+                                <option value="">Seleccione...</option>
+                                @foreach ($categories as $c)
+                                    <option value="{{ $c->id }}" {{ old('measurement_unit_id') == $c->id ? 'selected' : '' }}>{{ $c->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error('category_id')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
+
+                        {!! Form::label('UNSPSC_code', 'Codigo', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="far fa-keyboard"></i>
+                                </span>
+                            </div>
+                            {!! Form::number('UNSPSC_code', null, ['class' => 'form-control']) !!}
+                        </div>
+                        @error('UNSPSC_code')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
+
+                        {!! Form::label('kind_of_purchase_id', 'Tipo de Compra', ['class' => 'mt-3']) !!}
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-list"></i>
+                                </span>
+                            </div>
+                            <select name="kind_of_purchase_id" class="form-select" required>
+                                <option value="">Seleccione..</option>
+                                @foreach ($kind_of_purchase as $kp)
+                                    <option value="{{ $kp->id }}" {{ old('measurement_unit_id') == $kp->id ? 'selected' : '' }}>{{ $kp->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @error('kind_of_purchase_id')
+                            <div class="alert alert-danger py-0 my-1">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
+            </div>
+            <div class="card-footer bg-white text-right">
+                <a href="{{ route('cafeto.element.index') }}" class="btn btn-sm btn-light mr-2">
+                    <strong>Cancelar</strong>
+                </a>
+                    <button type="submit" class="btn btn-sm btn-success" id="btn-register-element">
+                        <b>Guardar</b>
+                    </button>
+            </div>
+        </div>
     {!! Form::close() !!}
 @endsection
 
 @push('scripts')
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script>
-    $(document).ready(function (e) {
-        $('#image').change(function () {
-            let reader = new FileReader();
-            reader.onload = (e) => {
-                $('#imagenSeleccionada').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(this.files[0]);
+    <!-- Recursos para los formatedores de datos -->
+    <script src="{{ asset('libs/cleave.js-1.6.0/dist/cleave.js') }}"></script>
+    <!-- Formateadores de datos -->
+    <script src="{{ asset('modules/cafeto/js/data-formats.js') }}"></script>
+
+    <script>
+        $(document).ready(function (e) {
+            // Obtener la URL de la imagen predeterminada
+            var defaultImageSrc = $('#imagenSeleccionada').attr('src');
+            $('#image').change(function () {
+                let reader = new FileReader();
+                reader.onload = (e) => {
+                    $('#imagenSeleccionada').attr('src', e.target.result);
+                }
+                // Verificar si se seleccionó una imagen
+                if (this.files && this.files[0]) {
+                    reader.readAsDataURL(this.files[0]);
+                } else {
+                    // Si no se selecciona una imagen, restaurar la imagen predeterminada
+                    $('#imagenSeleccionada').attr('src', defaultImageSrc);
+                }
+            });
+
+            // Desactivar botón de registrar cuando se envíe el formulario
+            $("#form-element").submit(function() {
+                $("#btn-register-element").prop("disabled", true); // Deshabilitar el botón
+            });
         });
-    });
-</script>
+    </script>
 @endpush
