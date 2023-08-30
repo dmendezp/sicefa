0719c8ba2d3a410660c29f08ad4e971a7c57f181
JuanDR403 <morricito.cerquera@gmail.com>
2023-08-29 21:32:45 -0500

    BIENESTAR - Modificaciones y definiciones necesarias para el modelo BenefitsTypesOfBenefits.php

--- Modules/BIENESTAR/Entities/BenefitsTypesOfBenefits.php
+++ Modules/BIENESTAR/Entities/BenefitsTypesOfBenefits.php
@@ -5,14 +5,32 @@ namespace Modules\BIENESTAR\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class benefits_types_of_benefits extends Model
+class BenefitsTypesOfBenefits extends Model
+
 {
     use HasFactory;
 
-    protected $fillable = [];
+    // Especifica la tabla asociada al modelo
+    protected $table = 'benefits_types_of_benefits'; // Reemplaza 'nombre_de_la_tabla' por el nombre real
+    
+    // Especifica los campos que pueden ser asignados masivamente
+    protected $fillable = [
+        'benefit_id',
+        'type_of_benefit_id',
+        // Agrega aquí otros campos si los tienes en la tabla
+    ];
     
     protected static function newFactory()
     {
         return \Modules\BIENESTAR\Database\factories\BenefitsTypesOfBenefitsFactory::new();
+    }
+    public function benefit()
+    {
+        return $this->belongsTo(Benefits::class, 'benefit_id');
+    }
+
+    public function typeOfBenefit()
+    {
+        return $this->belongsTo(TypesOfBenefits::class, 'type_of_benefit_id');
     }
 }
