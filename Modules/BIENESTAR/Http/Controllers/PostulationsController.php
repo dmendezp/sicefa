ca971357fd6cb3a1c7e6fb1df6f37efda5d8ada6
JuanDR403 <morricito.cerquera@gmail.com>
2023-09-01 23:40:00 -0500

    BIENESTAR - Integracion de funciones para el buen funcionamiento de la vista y las rutas

--- Modules/BIENESTAR/Http/Controllers/PostulationsController.php
+++ Modules/BIENESTAR/Http/Controllers/PostulationsController.php
@@ -10,6 +10,9 @@ use Modules\BIENESTAR\Entities\Convocations;
 use Modules\SICA\Entities\Apprentice;
 use Modules\BIENESTAR\Entities\TypesOfBenefits;
 use Modules\BIENESTAR\Entities\Questions;
+use Modules\BIENESTAR\Entities\benefits;
+
+
 
 class PostulationsController extends Controller
 {
@@ -19,6 +22,7 @@ class PostulationsController extends Controller
      */
     public function index()
     {
+        $benefits = Benefit::all();
         $postulations = Postulations::with(['apprentice', 'convocation', 'typesOfBenefits'])->get();
         return view('bienestar::postulations', compact('postulations'));
     }
@@ -39,6 +43,99 @@ class PostulationsController extends Controller
         
         return view('bienestar::postulations.modal', compact('postulation', 'questions'));
     }
+
+    public function updateScore(Request $request, $id)
+{
+    try {
+        // Buscar la postulación por ID
+        $postulation = Postulations::findOrFail($id);
+        
+        // Validar el valor del nuevo puntaje (puede agregar más validaciones según sus necesidades)
+        $request->validate([
+            'new-score' => 'required|integer', // Validar que sea un número entero
+        ]);
+
+        // Obtener el nuevo puntaje del formulario
+        $newScore = $request->input('new-score');
+
+        // Actualizar el puntaje de la postulación
+        $postulation->total_score = $newScore;
+        $postulation->save();
+
+        // Redirigir de vuelta a la página anterior con un mensaje de éxito (puede personalizar esto)
+        return redirect()->back()->with('success', 'Puntuación actualizada con éxito');
+    } catch (\Exception $e) {
+        // Capturar y manejar errores, puedes personalizar esto según tus necesidades
+        return redirect()->back()->with('error', 'Error al actualizar la puntuación: ' . $e->getMessage());
+    }
+}
+
+public function markBeneficiaries(Request $request)
+{
+    try {
+        // Obtener los IDs de las postulaciones seleccionadas desde el formulario
+        $selectedPostulations = $request->input('selectedPostulations');
+        
+        // Validar que al menos una postulación haya sido seleccionada
+        if (empty($selectedPostulations)) {
+            return response()->json(['error' => 'No se han seleccionado postulaciones'], 400);
+        }
+
+        // Obtener los datos comunes para todos los registros
+        $benefitId = $request->input('benefit_id');
+        $state = $request->input('state');
+        $message = $request->input('message');
+
+        // Recorrer las postulaciones seleccionadas y crear registros en postulations_benefits
+        foreach ($selectedPostulations as $postulationId) {
+            PostulationsBenefits::create([
+                'postulation_id' => $postulationId,
+                'benefit_id' => $benefitId,
+                'state' => $state,
+                'message' => $message,
+            ]);
+        }
+
+        return response()->json(['message' => 'Postulaciones marcadas como beneficiarias con éxito']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Error al marcar las postulaciones como beneficiarias', 'message' => $e->getMessage()], 500);
+    }
+}
+    public function assignBenefits(Request $request)
+{
+    try {
+        // Obtener las ID de postulaciones seleccionadas desde el formulario
+        $selectedPostulations = explode(',', $request->input('selected-postulations'));
+
+        // Obtener los valores de benefit_id, state y message desde el formulario
+        $benefitId = $request->input('benefit_id');
+        $state = $request->input('state');
+        $message = $request->input('message');
+
+        // Aquí puedes iterar sobre las postulaciones seleccionadas y registrar los valores en postulations_benefits
+        foreach ($selectedPostulations as $postId) {
+            $postulationBenefit = new PostulationBenefit([
+                'postulation_id' => $postId,
+                'benefit_id' => $benefitId,
+                'state' => $state,
+                'message' => $message,
+            ]);
+            $postulationBenefit->save();
+        }
+
+        return response()->json(['message' => 'Beneficios asignados con éxito']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Error al asignar beneficios', 'message' => $e->getMessage()], 500);
+    }
+}
+
+
+
+
+
+
+
+
 
 
     /**
