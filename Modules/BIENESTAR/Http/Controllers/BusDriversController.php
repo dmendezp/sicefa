ecccc92546c9db9c242ebf0ef8eff2a691c2753c
Mauroreyes04 <carlosmauriciopolancoreyes@gmail.com>
2023-09-01 21:20:32 -0500

    BIENESTAR - Funcionalidad de vista:Drivers_view

--- Modules/BIENESTAR/Http/Controllers/BusDriversController.php
+++ Modules/BIENESTAR/Http/Controllers/BusDriversController.php
@@ -5,6 +5,7 @@ namespace Modules\BIENESTAR\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\BIENESTAR\Entities\BusDrivers;
 
 class BusDriversController extends Controller
 {
@@ -12,19 +13,67 @@ class BusDriversController extends Controller
      * Display a listing of the resource.
      * @return Renderable
      */
-    public function index()
-    {
-        return view('bienestar::index');
+    public function Drivers_view()
+    {   
+        $busdrivers = BusDrivers::all();
+        return view('bienestar::Drivers_view',['busdrivers'=>$busdrivers]);
     }
+    public function Drivers_viewAdd(Request $request)
+    {
+
+        $name = $request->input('namedriver');
+        $email= $request->input('email');
+        $phone= $request->input('phone');
+
+        BusDrivers::create([
+            'name'=>$name,
+            'email'=>$email,
+            'phone'=>$phone,
+        ]);
+        return redirect()->route('bienestar.Drivers_view')->with('succes','Se ha agregado con exito!!');
+    }
+
+
 
     /**
      * Show the form for creating a new resource.
      * @return Renderable
      */
-    public function create()
+    public function Drivers_viewUp(Request $request, $id)
     {
-        return view('bienestar::create');
+            // Validar los datos del formulario si es necesario
+        
+            // Obtener el beneficio que deseas actualizar
+            $benefit = BusDrivers::find($id);
+        
+            // Actualizar los datos
+            $benefit->name = $request->input('name');
+            $benefit->email = $request->input('email');
+            $benefit->phone = $request->input('phone');
+            $benefit->save();
+        
+            // Redirigir o devolver una respuesta según tus necesidades
+            return redirect()->route('bienestar.Drivers_view')->with('success', 'Beneficio actualizado con éxito');
+     
     }
+    public function delete(Request $request, $id)
+    {
+        // Obtener el conductor que deseas eliminar
+        $busdriver = BusDrivers::find($id);
+        
+    
+        // Verificar si el conductor existe
+        if (!$busdriver) {
+            return redirect()->route('bienestar.Drivers_view')->with('error', 'El conductor no existe.');
+        }
+    
+        // Eliminar el conductor
+        $busdriver->delete();
+    
+        // Redirigir con un mensaje de éxito
+        return redirect()->route('bienestar.Drivers_view')->with('success', 'Conductor eliminado con éxito');
+    }
+    
 
     /**
      * Store a newly created resource in storage.
