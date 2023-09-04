1a82c1e2a2dc9a110da0cc559b69bf3b0c264bac
jarygaray12 <jarylisethgarayfajardo@gmail.com>
2023-09-03 21:20:22 -0500

    SENAEMPRESA -botones de agregar editar y eliminar

--- Modules/SENAEMPRESA/Resources/views/Company/SENAEMPRESA/senaempresa.blade.php
+++ Modules/SENAEMPRESA/Resources/views/Company/SENAEMPRESA/senaempresa.blade.php
@@ -29,18 +29,46 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Estrategia 34</td>
-                                        <td>Analisis</td>
-                                        <td>Editar|Eliminar</td>
-                                    </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Estrategia 34</td>
+                                    <td>Analisis</td>
+                                    <td>
+                                        <a href="#" class="btn btn-success">Agregar</a>
+                                        <a href="" class="btn btn-success">Editar</a>
+                                        <a href="#" class="btn btn-success">Eliminar</a>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
+        <h1>Eliminar Nota</h1>
+
+  <p>¿Estás seguro de que deseas eliminar esta nota?</p>
+
+  <button onclick="eliminarNota()">Eliminar</button>
+
+  <script>
+    function eliminarNota() {
+      var urlParams = new URLSearchParams(window.location.search);
+      var notaId = urlParams.get("id");
+
+      if (notaId !== null) {
+        var notas = JSON.parse(localStorage.getItem("notas")) || [];
+
+        if (notaId >= 0 && notaId < notas.length) {
+          notas.splice(notaId, 1);
+          localStorage.setItem("notas", JSON.stringify(notas));
+        }
+      }
+
+      window.location.href = "calificaciones.html";
+    }
+  </script>
+
 
         @section('content')
         @show
@@ -63,6 +91,9 @@
 
     @section('dataTables')
     @show
+
+
 </body>
 
 </html>
+
