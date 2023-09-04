acf15564dcc4c6b79c6da978347c683b4ac9a1ef
JuanDR403 <morricito.cerquera@gmail.com>
2023-09-04 10:32:16 -0500

    BIENESTAR - Correccion de conflictos

--- Modules/BIENESTAR/Routes/web.php
+++ Modules/BIENESTAR/Routes/web.php
@@ -42,10 +42,10 @@ Route::prefix('/bienestar')->group(function() {
     Route::post('/benefits/update/{id}', 'BenefitsController@update')->name('benefits.update');
 
      //Vista CRUD Buses
-    Route::get('/buses', 'BusesController@index')->name('bienestar.buses');
-    Route::post('/buses/store', 'BusesController@store')->name('bienestar.buses.store');
-    Route::delete('/buses/delete/{id}', 'BusesController@destroy')->name('bienestar.buses.destroy');
-    Route::put('/buses/update/{id}', 'BusesController@update')->name('bienestar.buses.update');
+     Route::get('/buses', 'BusesController@index')->name('bienestar.buses');
+     Route::post('/buses/store', 'BusesController@store')->name('bienestar.buses.store');
+     Route::delete('/buses/delete/{id}', 'BusesController@destroy')->name('bienestar.buses.destroy');
+     Route::put('/buses/update/{id}', 'BusesController@update')->name('bienestar.buses.update');
 
       // Vista CRUD tipo de beneficios
     Route::get('/typeofbenefits', 'TypesOfBenefitsController@typeofbenefits')->name('bienestar.typeofbenefits');
@@ -61,24 +61,34 @@ Route::prefix('/bienestar')->group(function() {
     Route::put('/benefitstypeofbenefits/{id}', 'BenefitsTypesOfBenefitsController@update')->name('bienestar.benefitstypeofbenefits.update');
     Route::delete('/benefitstypeofbenefits/{id}', 'BenefitsTypesOfBenefitsController@destroy')->name('benefitstypeofbenefits.destroy');
 
-    //vista postulation
+    // Rutas para la vista postulation
     Route::get('/postulations', 'PostulationsController@index')->name('bienestar.postulations.index');
     Route::get('/postulations/{id}', 'PostulationsController@show')->name('bienestar.postulations.show');
     Route::get('/postulations/modal/{id}', 'PostulationsController@showModal')->name('bienestar.postulations.modal');
+    Route::post('/update-postulation-score/{id}', 'PostulationsController@updateScore')->name('bienestar.postulations.update-score');
+    Route::post('/mark-beneficiaries', 'TuController@markBeneficiaries')->name('bienestar.mark-beneficiaries');
+    Route::post('/assign-benefits', 'PostulationsController@assignBenefits')->name('bienestar.postulations.assign-benefits');
+    Route::get('/convocatoria', 'ConvocationsController@convocatoria')->name('convocatoria');
+    Route::post('/bienestar/assign-benefits', 'PostulationsController@assignBenefits')->name('bienestar.postulations.assign-benefits');
+    Route::put('bienestar/postulations/update-state/{id}', 'PostulationsController@updateState')->name('bienestar.postulations.update-state');
+    Route::post('/bienestar/postulations/create', 'PostulationsController@create')->name('bienestar.postulations.create');
+    Route::post('bienestar/postulations/mark-as-beneficiaries', 'PostulationsController@markAsBeneficiaries')->name('bienestar.postulations.mark-as-beneficiaries');
 
-
-        //Vistas Rutas de transporte
+    
+    
+    //Vistas Rutas de transporte
     Route::get('/transportroutes','RoutesTransportationsController@transportroutes')->name('bienestar.transportroutes');
     Route::post('/transportroutes/add','RoutesTransportationsController@transportroutesAdd')->name('bienestar.transportroutes.add');
     Route::get('/LisRutas', 'RoutesTransportationsController@LisRutas')->name('bienestar.LisRutas');
 
     // vista de conductores
-    Route::get('/Drivers_view', 'BIENESTARController@Drivers_view')->name('bienestar.Drivers_view');
+    Route::get('/Drivers_view', 'BusDriversController@Drivers_view')->name('bienestar.Drivers_view');
+    Route::post('/Drivers_view/add', 'BusDriversController@Drivers_viewAdd')->name('bienestar.Driversw.add');
+    Route::put('/Drivers_view/update/{id}', 'BusDriversController@Drivers_viewUp')->name('bienestar.Drivers.update');
+    Route::delete('/Drivers_view/delete/{id}', 'BusDriversController@delete')->name('bienestar.Drivers.delete');
 
-     // vista de gestiones de convocatorias
+    // vista de gestiones de convocatorias
     Route::get('/Convocations', 'ConvocationsController@index')->name('bienestar.Convocations');
     Route::post('/Convocations/store', 'ConvocationsController@store')->name('bienestar.Convocations.store');
     Route::put('/Convocations/update/{id}', 'ConvocationsController@update')->name('bienestar.Convocations.update');
 });
-
-  
