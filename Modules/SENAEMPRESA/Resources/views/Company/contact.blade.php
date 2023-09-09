97379a615abb20e8a7c52f913e860393bd1633fc
Junior6580 <jsmedinah5873@gmail.com>
2023-09-09 14:37:06 -0500

    SENAEMPRESA - Actualización de vista Contacto

--- Modules/SENAEMPRESA/Resources/views/Company/contact.blade.php
+++ Modules/SENAEMPRESA/Resources/views/Company/contact.blade.php
@@ -16,11 +16,10 @@
                         <div class="card-body box-profile">
                             <div class="text-center">
                                 <img class="profile-user-img img-fluid img-circle"
-                                    src="{{ asset('storage/photos/shares/people/Diego.png') }}" alt="User profile picture">
+                                    src="{{ asset('senaempresa/images/Contacto/Medina.jpg') }}" alt="User profile picture">
                             </div>
-                            <h3 class="profile-username text-center">Diego Andres Mendez Pastrana</h3>
-                            <p class="text-muted text-center">Instructor - Tgo. Analisis y desarollo de sistemas de
-                                información</p>
+                            <h3 class="profile-username text-center">Junior Stiven Medina Hernandez</h3>
+                            <p class="text-muted text-center">Aprendiz - Tgo. Analisis y desarollo de software</p>
                             <ul class="list-group list-group-unbordered mb-3">
                                 <li class="list-group-item">
                                     <b>Facebook</b> <a href="#" class="btn btn-outline-primary float-right"><b><i
@@ -36,6 +35,7 @@
                 </div>
 
                 <div class="col-lg-4">
+
                     <div class="card card-primary card-outline shadow">
                         <div class="card-header">
                             <h5 class="m-0">{{ $title }}</h5>
@@ -43,11 +43,66 @@
                         <div class="card-body box-profile">
                             <div class="text-center">
                                 <img class="profile-user-img img-fluid img-circle"
-                                    src="{{ asset('sica/images/logofs.png') }}" alt="User profile picture">
+                                    src="{{ asset('senaempresa/images/Contacto/Marin.jpg') }}" alt="User profile picture">
                             </div>
-                            <h3 class="profile-username text-center">Fabrica de Software</h3>
-                            <p class="text-muted text-center">Unidad productiva - Tgo. Analisis y desarollo de sistemas de
-                                información</p>
+                            <h3 class="profile-username text-center">Jennifer Marin Montealegre</h3>
+                            <p class="text-muted text-center">Aprendiz - Tgo. Analisis y desarollo de software</p>
+                            <ul class="list-group list-group-unbordered mb-3">
+                                <li class="list-group-item">
+                                    <b>Facebook</b> <a href="#" class="btn btn-outline-primary float-right"><b><i
+                                                class="fab fa-facebook-f"></i></b></a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>LinkedIn</b> <a href="#" class="btn btn-outline-primary float-right"><b><i
+                                                class="fab fa-linkedin-in"></i></b></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row  justify-content-md-center">
+
+                <div class="col-lg-4">
+
+                    <div class="card card-primary card-outline shadow">
+                        <div class="card-header">
+                            <h5 class="m-0">{{ $title }}</h5>
+                        </div>
+                        <div class="card-body box-profile">
+                            <div class="text-center">
+                                <img class="profile-user-img img-fluid img-circle"
+                                    src="{{ asset('senaempresa/images/Contacto/Penagos.jpg') }}" alt="User profile picture">
+                            </div>
+                            <h3 class="profile-username text-center">Diego Alejandro Penagos Ninco</h3>
+                            <p class="text-muted text-center">Aprendiz - Tgo. Analisis y desarollo de software</p>
+                            <ul class="list-group list-group-unbordered mb-3">
+                                <li class="list-group-item">
+                                    <b>Facebook</b> <a href="#" class="btn btn-outline-primary float-right"><b><i
+                                                class="fab fa-facebook-f"></i></b></a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>LinkedIn</b> <a href="#" class="btn btn-outline-primary float-right"><b><i
+                                                class="fab fa-linkedin-in"></i></b></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+
+                    <div class="card card-primary card-outline shadow">
+                        <div class="card-header">
+                            <h5 class="m-0">{{ $title }}</h5>
+                        </div>
+                        <div class="card-body box-profile">
+                            <div class="text-center">
+                                <img class="profile-user-img img-fluid img-circle"
+                                    src="{{ asset('senaempresa/images/Contacto/Garay.jpg') }}" alt="User profile picture">
+                            </div>
+                            <h3 class="profile-username text-center">Jary Liceth Garay</h3>
+                            <p class="text-muted text-center">Aprendiz - Tgo. Analisis y desarollo de software</p>
                             <ul class="list-group list-group-unbordered mb-3">
                                 <li class="list-group-item">
                                     <b>Facebook</b> <a href="#" class="btn btn-outline-primary float-right"><b><i
