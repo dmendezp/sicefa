300c70c571f08e7eb0b1df49d702cf5bf10f6e6f
SrManuel-1 <stevenlievano7575@gmail.com>
2023-09-11 10:35:42 -0500

    CPD -Internacionalización estandarizada

---
+++ Modules/CPD/Resources/lang/en/monitoring.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    // Breadcrumbs
+    'Breadcrumb_Monitoring'                 => 'Monitorings',
+    'Breadcrumb_Active_Monitoring_Register' => 'Register',
+    'Breadcrumb_Active_Monitoring_Update'   => 'Update',
+
+    // Table Monitorings
+    // Btn Table Monitorings
+    'TB_Show_Hide'            => 'Show / Hide',
+    'TB_Physicochemist'       => 'Physicochemist',
+    'TB_Biota'                => 'Biota',
+    'TB_Farming'              => 'Farming',
+    'TB_Weather'              => 'Weather',
+    'TB_All'                  => 'All',
+    // Title Table Monitorings
+    '1T_Number'                => 'N°',
+    '1T_Producer'              => 'Producer',
+    '1T_Monitoring'            => 'Monitoring',
+    '1T_Municipality_Village'  => 'Municipality / Village',
+    '1T_Typology'              => 'Tipology',
+    '1T_Altitude'              => 'Altitude',
+    '1T_Actions'               => 'Actions',   
+    
+    // Form  
+    'Form_Title_Monitoring_Dates' => 'General monitoring data',
+    'F_Text_Producer'             => 'Producer: ',
+    'F_Text_Village'              => 'Village: ',
+    'F_Text_Monitoring'           => 'Monitoring: ',
+    'F_Placeholder_Year'          => 'Year',
+    'F_Text_Typology'             => 'Typology: ',
+    'F_Text_Altitude'             => 'Altitude: ',
+    // Select
+    'F_Select'                    => 'Select... ',
+    'F_Search_Text_1'             => 'No results found',
+    'F_Search_Text_2'             => 'Searching...',
+    
+    // Tooltips
+    'T_Register_Monitoring'    => 'Register Monitoring',
+    'T_Cancel_Register'        => 'Cancel Register',
+    'T_Edit_Monitoring'        => 'Edit Monitoring',
+    'T_Cancel_Edit'            => 'Cancel Update',
+    'T_View_Monitoring'        => 'View Monitoring',
+    'T_Edit_Monitoring'        => 'Edit Monitoring',
+    'T_Delete_Monitoring'      => 'Delete Monitoring',
+    'T_Year_of_monitoring'     => 'Year of monitoring',
+    'T_Type_Farming'           => 'Type of farming',
+    'T_Meters_Sea_Level'       => 'Meters above sea level',
+    
+    // Alerts - Sweetalert2
+    'S_Title_Success_Register' => 'Monitoring successfully registered.',
+    'S_Title_Fail_Register'    => 'Monitoring could not be registered.',
+    'S_Title_Success_Edit'     => 'Monitoring successfully update.',
+    'S_Title_Fail_Edit'        => 'Monitoring could not be update.',
+    'S_Title_Success_Delete'   => 'Monitoring successfully delete.',
+    'S_Title_Fail_Delete'      => 'Monitoring could not be delete.',
+    
+    // Btns
+    'Btn_Cancel' => 'Cancel',
+    'Btn_Save'   => 'Save',
+    'Btn_Close'  => 'Close',
+    'Btn_Delete' => 'Delete',
+
+];
