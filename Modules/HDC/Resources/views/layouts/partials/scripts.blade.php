f752a163f7b8ca2cc3dffa8d655a1351b7109070
Sofia032 <osorioleidysofia@gmail.com>
2023-09-12 11:53:45 -0500

    HDC - Se Anexa Vista Grafica

--- Modules/HDC/Resources/views/layouts/partials/scripts.blade.php
+++ Modules/HDC/Resources/views/layouts/partials/scripts.blade.php
@@ -4,3 +4,5 @@
 <script src="{{ asset('AdminLTE/dist/js/adminlte.min.js') }}"></script>
 <!-- Boostrap-5.3.0 -->
 <script src="{{ asset('libs/Bootstrap-5.3.0-alpha/js/bootstrap.bundle.min.js')}}"></script>
+<!-- chart.js -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
