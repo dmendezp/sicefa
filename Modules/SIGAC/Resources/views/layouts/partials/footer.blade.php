11230f83a3992ce4c6f73a03a691f467b2b0d158
JuanDR403 <morricito.cerquera@gmail.com>
2023-09-12 10:48:42 -0500

    BIENESTAR - Eliminacion de SIGAC en el Proyecto, y modificacion y creacion de archivos

--- Modules/SIGAC/Resources/views/layouts/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      <b>Version Alpha</b> 1.0 
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2023 <a href="{{ route('cefa.welcome') }}" style="text-decoration: none">Centro de Fomación Agroindustrial "La Angostura"</a>.</strong> {{trans('sigac::general.All Rights Reserved.')}}
-</footer>
