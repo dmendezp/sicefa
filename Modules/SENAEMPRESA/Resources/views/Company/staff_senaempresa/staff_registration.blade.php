93a26e68d8d4fd2d6f2a66df29d0f0772fe4b45e
diego222709 <diegopenagos955@gmail.com>
2023-09-13 20:32:50 -0500

    SENAEMPRESA - Staff arreglo de editar,SweetAlert y requerimientos

--- Modules/SENAEMPRESA/Resources/views/Company/staff_senaempresa/staff_registration.blade.php
+++ Modules/SENAEMPRESA/Resources/views/Company/staff_senaempresa/staff_registration.blade.php
@@ -1,52 +1,71 @@
 @extends('senaempresa::layouts.master')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-primary card-outline shadow">
-                    <div class="card-header">{{ $title }}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card card-primary card-outline shadow">
+                <div class="card-header">{{ $title }}</div>
 
-                    <div class="card-body">
-                        <form action="{{ route('company.senaempresa.nuevo_personal') }}" method="POST"
-                            enctype="multipart/form-data">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="position_company_id" class="form-label">Id Cargo</label>
-                                <select class="form-control" name="position_company_id" aria-label="Selecciona un Cargo">
-                                    <option value="" selected>Selecciona un Cargo</option>
-                                    @foreach ($PositionCompany as $positionCompany)
-                                        <option value="{{ $positionCompany->id }}">
-                                            {{ $positionCompany->id }}
-                                            {{ $positionCompany->description }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="apprentice_id" class="form-label">Id Aprendiz</label>
-                                <select class="form-control" name="apprentice_id" aria-label="Selecciona un Aprendiz">
-                                    <option value="" selected>Selecciona un Aprendiz</option>
-                                    @foreach ($Apprentices as $Apprentice)
-                                        <option value="{{ $Apprentice->id }}">
-                                            {{ $Apprentice->Person->document_number }}
-                                            {{ $Apprentice->Person->first_name }}
-                                            {{ $Apprentice->Person->first_last_name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="image" class="form-label">Imgen Personal</label><br>
-                                <input type="file" id="image" name="image">
-                            </div>
-                            <button type="submit" class="btn btn-success">Agregar</button>
-                            <a href="{{ route('company.senaempresa.personal') }}" class="btn btn-danger btn-xl">Cancelar</a>
-                        </form>
-                    </div>
+                <div class="card-body">
+                    <form action="{{ route('company.senaempresa.nuevo_personal') }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="position_company_id" class="form-label">Id Cargo</label>
+                            <select class="form-control" name="position_company_id" aria-label="Selecciona un Cargo" required>
+                                <option value="" selected>Selecciona un Cargo</option>
+                                @foreach ($PositionCompany as $positionCompany)
+                                    <option value="{{ $positionCompany->id }}">
+                                        {{ $positionCompany->id }}
+                                        {{ $positionCompany->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="apprentice_id" class="form-label">Buscar Aprendiz por Documento o Nombre</label>
+                            <input type="text" class="form-control" id="search-input" name="search-input" placeholder="Ingresar número o nombre">
+                            <select class="form-control" name="apprentice_id" aria-label="Selecciona un Aprendiz" id="apprentice-select" multiple="multiple" required>
+                                @foreach ($Apprentices as $Apprentice)
+                                <option value="{{ $Apprentice->id }}">
+                                    {{ $Apprentice->Person->document_number }}
+                                    {{ $Apprentice->Person->first_name }}
+                                    {{ $Apprentice->Person->first_last_name }}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
 
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Imagen Personal</label><br>
+                            <input type="file" id="image" name="image" required>
+                        </div>
+                        <button type="submit" class="btn btn-success">Agregar</button>
+                        <a href="{{ route('company.senaempresa.personal') }}" class="btn btn-danger btn-xl">Cancelar</a>
+                    </form>
                 </div>
             </div>
         </div>
-    </div><br>
+    </div>
+</div><br>
+
 @endsection
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const searchInput = document.getElementById("search-input");
+        const apprenticeSelect = document.getElementById("apprentice-select");
+
+        searchInput.addEventListener("input", function () {
+            const searchText = this.value.trim().toLowerCase();
+            
+            for (let option of apprenticeSelect.options) {
+                const optionText = option.text.toLowerCase();
+                const isMatch = optionText.includes(searchText);
+                option.hidden = !isMatch;
+            }
+        });
+    });
+</script>
--- Modules/SENAEMPRESA/Resources/views/layouts/structure/scripts.blade.php
+++ Modules/SENAEMPRESA/Resources/views/layouts/structure/scripts.blade.php
@@ -49,6 +49,12 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 <!-- {{-- Sweatalert and toast --}} -->
 <script src="{{ asset('AdminLTE/plugins/toastr/toastr.min.js') }}"></script>
 
