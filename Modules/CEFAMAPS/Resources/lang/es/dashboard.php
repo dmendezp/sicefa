cc1f20d481840670277a99b35226bb499ff2ce4f
JDGM0331 <jesusdavidguevaramunar123@gmail.com>
2023-09-17 10:26:05 -0500

    SICEFA - Restauración de recursos para eliminación de conflictos

---
+++ Modules/CEFAMAPS/Resources/lang/es/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    // Breadcrumbs
+    'Breadcrumb_CEFAMAPS_Admin' => 'Administrador',
+    
+    // --- Section Card Configuration ---
+    'Title_Card_Settings'     => 'Configuración de CEFAMAPS',
+    'Title_Card_Sectors'      => 'Sectores',
+    'Title_Card_Units'        => 'Unidades',
+    'Title_Card_Environments' => 'Ambientes',
+    'Title_Card_Pages'        => 'Páginas',
+    
+    // --- Section Alert Welcome User ---
+    'Title_Alert' => 'Bienvenido',
+];
