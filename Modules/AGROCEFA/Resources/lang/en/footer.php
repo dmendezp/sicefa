0e4f7bb5c599e92b01b2ead8b13b980308db5e1c
DerlySapuy27 <sapuyderly2002@gmail.com>
2023-09-17 21:09:37 -0500

    AGROCEFA-Actualizacion de footer y traduccion

---
+++ Modules/AGROCEFA/Resources/lang/en/footer.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'ExitForm' => 'Exit Form',
+    'English' => 'English',
+    'Spanish' => 'Spanish',
+    'Parameters' => 'Parameters',
+
+    '"La Angostura" Agro-Industrial Training Center' => '"La Angostura" Agro-Industrial Training Center',
+    'Start' => 'Start',
+    'Developers' => 'Developers',
+    'About AGROCEFA' => 'About AGROCEFA',
+    'Reports' => 'Reports',
+    'Farming Data With Code!' => 'Farming Data With Code!',
+    'All rights reserved' => 'All rights reserved',
+    
+
+];
---
+++ Modules/AGROCEFA/Resources/lang/es/footer.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'ExitForm' => 'Exit Form',
+    'English' => 'English',
+    'Spanish' => 'Spanish',
+    'Parameters' => 'Parameters',
+
+    '"La Angostura" Agro-Industrial Training Center' => 'Centro de Formación AgroIndustrial "La Angostura"',
+    'Start' => 'Inicio',
+    'Developers' => 'Desarrolladores',
+    'About AGROCEFA' => 'Sobre AGROCEFA',
+    'Reports' => 'Reportes',
+    'Farming Data With Code!' => '¡Cultivando Datos Con Código!',
+    'All rights reserved' => 'Derechos Reservados',
+    
+
+];
