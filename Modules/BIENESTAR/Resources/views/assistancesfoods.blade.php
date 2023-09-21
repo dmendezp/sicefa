92b62025c0875f6d684290044b0151eafeb68d53
saira2003 <guevarax72@gmail.com>
2023-09-21 11:51:26 -0500

    BIENESTAR - Ajuste en vista asistencias del beneficio de alimentación

--- Modules/BIENESTAR/Resources/views/assistancesfoods.blade.php
+++ Modules/BIENESTAR/Resources/views/assistancesfoods.blade.php
@@ -1,13 +1,14 @@
 @extends('bienestar::layouts.master')
 
-<div class="container-fluid">
-    <h1>Listado de Aprendices Afiliados <i class="fas fa-clipboard-list"></i></h1>
-    <div class="row justify-content-md-center pt-4">
-        <div class="card shadow col-md-8">
-            <!-- Resto de tu contenido -->
+@section('content')
+    <div class="container-fluid">
+        <h1>Listado de Aprendices Afiliados <i class="fas fa-clipboard-list"></i></h1>
+        <div class="row justify-content-md-center pt-4">
+            <div class="card shadow col-md-8">
+                <!-- Resto de tu contenido -->
+            </div>
         </div>
     </div>
-</div>
 @endsection
 
 
