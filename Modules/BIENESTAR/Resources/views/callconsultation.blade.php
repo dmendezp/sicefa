7b57d954bbaa1e8014d0eb6916476560cebb3999
Kevinvaldes77 <santiagovaldez1214@gmail.com>
2023-09-22 08:30:32 -0500

    BIENESTAR - Campo De Entrada Numerico

--- Modules/BIENESTAR/Resources/views/callconsultation.blade.php
+++ Modules/BIENESTAR/Resources/views/callconsultation.blade.php
@@ -10,6 +10,24 @@
                     </div>
                     <div class="box-body">
                         <!-- Contenido de la vista -->
+
+                        <div class="card">
+                            <div class="card-header">
+                                <!-- Encabezado del card -->
+                            </div>
+                            <div class="card-body text-center"> <!-- Centramos el contenido en el card verticalmente -->
+                                <form>
+                                    <div class="form-group">
+                                        <label for="numero_documento">Número de Documento</label>
+                                        <div class="col-md-6 mx-auto"> <!-- Centramos el campo numérico horizontalmente -->
+                                            <input type="number" class="form-control" id="numero_documento" name="numero_documento">
+                                        </div>
+                                    </div>
+                                    <!-- Otros campos y botones de formulario si es necesario -->
+                                </form>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
