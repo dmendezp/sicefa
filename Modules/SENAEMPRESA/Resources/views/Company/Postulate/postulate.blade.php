33288bdf175ba01ee21614a98c8e87a011f4ffd5
Jennyferm05 <jennyfermarin05@gmail>
2023-10-02 11:10:51 -0500

    SENAEMPRESA - Botones de descarga

--- Modules/SENAEMPRESA/Resources/views/Company/Postulate/postulate.blade.php
+++ Modules/SENAEMPRESA/Resources/views/Company/Postulate/postulate.blade.php
@@ -6,7 +6,7 @@
         <br>
         <div class="col-md-12">
             <div class="card card-primary card-outline shadow">
-                <div class="table-responsive">
+                <div class="card-body">
                     <table id="datatable" class="table table-striped table-bordered">
                         <thead>
                             <tr>
@@ -18,18 +18,32 @@
                                 <th>16 Personalidades</th>
                                 <th>Propuesta</th>
                                 <th>Puntaje Total</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($postulates as $postulate)
                                 <tr>
                                     <td>{{ $postulate->id }}</td>
-                                    <td>{{ $postulate->apprentice->person->first_name }} {{ $postulate->apprentice->person->first_last_name }}</td>
+                                    <td>{{ $postulate->apprentice->person->first_name }}
+                                        {{ $postulate->apprentice->person->first_last_name }}</td>
                                     <td>{{ $postulate->vacancy->name }}</td>
                                     <td>{{ $postulate->state }}</td>
-                                    <td>{{ $postulate->cv }}</td>
-                                    <td>{{ $postulate->personalities }}</td>
-                                    <td>{{ $postulate->proposal }}</td>
+                                    <td>
+                                        <a href="{{ asset('storage/' . $postulate->cv) }}" class="btn btn-primary" download>
+                                            <i class="fas fa-download fa-sm"></i> CV
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="{{ asset('storage/' . $postulate->personalities) }}" class="btn btn-primary" download>
+                                            <i class="fas fa-download fa-sm"></i> Personalidades
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="{{ asset('storage/' . $postulate->proposal) }}" class="btn btn-primary" download>
+                                            <i class="fas fa-download fa-sm"></i> Propuesta
+                                        </a>
+                                    </td>
                                     <td>0</td>
                                     <td> <a href="a" class="btn btn-primary">Asignar</a></td>
                                 </tr>
