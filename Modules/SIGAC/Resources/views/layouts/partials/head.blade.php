4b28dc52756ef4ca7eebb7791d4660c3c4ccbfc0
SrManuel-1 <stevenlievano7575@gmail.com>
2023-10-02 08:32:50 -0500

    SIGAC - Personalización de favicon

--- Modules/SIGAC/Resources/views/layouts/partials/head.blade.php
+++ Modules/SIGAC/Resources/views/layouts/partials/head.blade.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('modules/sigac/images/icon/graduation-cap-solid.ico') }}">
     <title>SIGAC | {{ $view['titlePage'] }}</title>
 
     <!-- Google Font: Source Sans Pro -->
