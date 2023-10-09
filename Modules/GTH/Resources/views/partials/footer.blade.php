6d594b5a7276d717011a8ec48b3b9dabb08d2014
danielguz10 <fg901224@gmail.com>
2023-10-09 12:28:32 -0500

    GTH - Implementación de Footer

---
+++ Modules/GTH/Resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      ADSI / ADSO <br>
+      <strong>Copyright &copy; 2014-2019 <a target="blank" href="https://centroagroindustrial.blogspot.com/">Centro de Formación Agroindustrial "La Angostura"</a>.</strong> All rights reserved.
+    </div>
+    <!-- Copyright -->
+  </footer>
