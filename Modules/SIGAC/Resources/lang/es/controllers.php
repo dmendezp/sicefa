06b0518755ad71c5f54ed4cb27426abc37714430
SrManuel-1 <stevenlievano7575@gmail.com>
2023-10-11 09:10:03 -0500

    SIGAC - Internacionalización de titulos de las funciones en el archivo controllers

--- Modules/SIGAC/Resources/lang/es/controllers.php
+++ Modules/SIGAC/Resources/lang/es/controllers.php
@@ -24,6 +24,33 @@ return [
 	'SIGAC_apprentice_dashboard_title_page' => 'Aprendiz',
 	'SIGAC_apprentice_dashboard_title_view' => 'Página principal',
 
+	// ------ApprenticeController------
+	// Send Excuses
+	'SIGAC_send_excuses_title_page' => 'Excusas',
+	'SIGAC_send_excuses_title_view' => 'Enviar Excusas',
+
+	// ------AttendanceController------
+	// Consult Excuses
+	'SIGAC_consult_excuses_title_page'     => 'Excusas',
+	'SIGAC_consult_excuses_title_view'     => 'Consultar Excusas',
+	// Consult Attendance
+	'SIGAC_consult_attendance_title_page'  => 'Asistencia',
+	'SIGAC_consult_attendance_title_view'  => 'Consultar Asistencia',
+	// Register Attendance
+	'SIGAC_register_attendance_title_page' => 'Asistencia',
+	'SIGAC_register_attendance_title_view' => 'Registrar Asistencia',
+	// Reports Attendance
+	'SIGAC_reports_attendance_title_page'  => 'Asistencia',
+	'SIGAC_reports_attendance_title_view'  => 'Reportes de Asistencia',
+
+	// -----ProgrammeController------
+	// Programming Schedules
+	'SIGAC_programming_schedules_title_page' => 'Programación de Horarios',
+	'SIGAC_programming_schedules_title_view' => 'Programación de Horarios',
+	// Event Programming
+	'SIGAC_event_programming_title_page' => 'Programación de Eventos',
+	'SIGAC_event_programming_title_view' => 'Programación de Eventos',
+
 	// -----ScheduleController------
 	// Schedule Instructor
 	'SIGAC_schedule_instructor_title_page' => 'Horario de Instructor',
@@ -34,12 +61,4 @@ return [
 	// Schedule Apprentice
 	'SIGAC_schedule_apprentice_title_page' => 'Horario de Aprendiz',
 	'SIGAC_schedule_apprentice_title_view' => 'Horario de Aprendiz',
-
-	// -----ProgrammeController------
-	// Programming Schedules
-	'SIGAC_programming_schedules_title_page' => 'Programación de Horarios',
-	'SIGAC_programming_schedules_title_view' => 'Programación de Horarios',
-	// Event Programming
-	'SIGAC_event_programming_title_page' => 'Programación de Eventos',
-	'SIGAC_event_programming_title_view' => 'Programación de Eventos',
 ];
