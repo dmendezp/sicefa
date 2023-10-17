7d92bb335a60d38718204aa46e238fb331057ced
SrManuel-1 <stevenlievano7575@gmail.com>
2023-10-17 15:21:36 -0500

    SIGAC - Internacionalización de la vista registro de asistencia

--- Modules/SIGAC/Resources/lang/es/attendance.php
+++ Modules/SIGAC/Resources/lang/es/attendance.php
@@ -1,43 +1,47 @@
 <?php
 
 return [
-    //Breadcrumb
+    // Breadcrumbs
 	'Breadcrumb_Active_Attendance'       => 'Asistencia',
 	'Breadcrumb_Register'                => 'Registro',
     'Breadcrumb_Attendance_Registration' => 'Registro de Asistencia',
     'Breadcrumb_Attendance_Consult'      => 'Attendance Consult',
 
-    //Card
-    'CardSubtitle1'            => 'Selecciona el ambiente',
-    'CardSubtitle2'            => 'Selecciona el tecnológo',
-    'Current date and time'   => 'Fecha y hora actual',
-    'Start of training'       => 'Inicio de la Formación',
-    'End of training'         => 'Fin de la Formación',
-    'Select...'               => 'Seleccione...',
-    'Register Previous'       => 'Registrar Anterior',
+    // Card Register Attendance
+    'Title_Card_Select_Environment'  => 'Selecciona el ambiente',
+    'Title_Card_Select_Technologist' => 'Selecciona el tecnológo',
+    'Title_Current_Date'             => 'Fecha y hora actual',
+    'Title_Start_Training'           => 'Inicio de la Formación',
+    'Title_End_training'             => 'Fin de la Formación',
+    'Select...'                      => 'Seleccione...',
 
-    //Table
-    'Code'               => 'Código',
-    'Name'               => 'Nombre',
-    'Document Number'    => 'Número de documento',
-    'Accumulated Faults' => 'Fallas acumuladas',
+    // Card Summary Session
+    'Title_Summary_Session'           => 'Resumen de la sesión',
+    'Text_Total_Present'              => 'Total de presentes:',
+    'Text_Total_Justified_Failures'   => 'Total de fallas justificadas:',
+    'Text_Total_Unjustified_Failures' => 'Total de fallas injustificadas:',
+    'Text_Total_Missing_Stockings'    => 'Total de media faltas:',
 
-    //Card Summary
-    'Summary of the session'      => 'Resumen de la sesión',
-    'Total present:'              => 'Total de presentes:',
-    'Total justified failures:'   => 'Total de fallas justificadas:',
-    'Total unjustified failures:' => 'Total de fallas injustificadas:',
-    'Total missing stockings:'    => 'Total de media faltas:',
-    'Legend'                      => 'Leyenda',
-    'Type'                        => 'Tipo',
-    'Quantity'                    => 'Cantidad',
+    // Modal Register Previous
+    'Title_Modal_Register_Previous'    => 'Registrar asistencia del dia anterior',
+
+    // Table with results
+    '1T1_Code'               => 'Código',
+    '1T2_Name'               => 'Nombre',
+    '1T3_Document_Number'    => 'Número de documento',
+    '1T4_Accumulated_Faults' => 'Fallas acumuladas',
+    '1T5_Attendance'         => 'Asistencia',
 
     // Attendance Consult
     'Title_Card_Consult'    => 'Consulta la asistencia haciendo clic en:',
     'Title_Card_Apprentice' => 'Aprendiz',
     'Title_Card_Titled'     => 'Titulada',
 
-    //Buttons
-    'Accept' => 'Aceptar',
-    'Close'  => 'Cerrar',
+    // Buttons
+    'Btn_Register_Previous' => 'Registrar Anterior',
+    'Btn_Accept'            => 'Aceptar',
+    'Btn_Close'             => 'Cerrar',
+    
+    // Exeptions
+    'E1_Text_No_Results'    => 'No hay datos disponibles, selecciona un tecnólogo',
 ];
--- Modules/SIGAC/Resources/views/attendance/register.blade.php
+++ Modules/SIGAC/Resources/views/attendance/register.blade.php
@@ -19,7 +19,7 @@
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="environment"
-                                    class="form-label">{{ trans('sigac::attendance.CardSubtitle1') }}</label>
+                                    class="form-label">{{ trans('sigac::attendance.Title_Card_Select_Environment') }}</label>
                                 <select id="environment" class="form-select" aria-label="Default select example">
                                     <option selected disabled>{{ trans('sigac::attendance.Select...') }}</option>
                                     <option value="#">Prueba</option>
@@ -27,7 +27,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="technologist"
-                                    class="form-label">{{ trans('sigac::attendance.CardSubtitle2') }}</label>
+                                    class="form-label">{{ trans('sigac::attendance.Title_Card_Select_Technologist') }}</label>
                                 <select name="course_id" class="form-select" aria-label="Default select example">
                                     <option value="">{{ trans('sigac::attendance.Select...') }}</option>
                                         <option value="#">
@@ -40,22 +40,22 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="mb-3">
-                                        <label for="producto">{{ trans('sigac::attendance.Current date and time') }}</label>
+                                        <label for="producto">{{ trans('sigac::attendance.Title_Current_Date') }}</label>
                                         <p class="card-text" id="fecha-hora"></p>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="start-training"
-                                            class="form-label">{{ trans('sigac::attendance.Start of training') }}</label>
-                                        <p id="start-training">16/05/2023 12:50</p>
+                                            class="form-label">{{ trans('sigac::attendance.Title_Start_Training') }}</label>
+                                        <p id="start-training">16/11/2023 07:30</p>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="end-training"
-                                            class="form-label">{{ trans('sigac::attendance.End of training') }}</label>
-                                        <p id="end-training">16/05/2023 12:50</p>
+                                            class="form-label">{{ trans('sigac::attendance.Title_End_training') }}</label>
+                                        <p id="end-training">16/11/2023 12:50</p>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -63,7 +63,7 @@
                                         <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                             data-bs-target="#exampleModal">
                                             <i class="fas fa-plus"></i>
-                                            {{ trans('sigac::attendance.Register Previous') }}
+                                            {{ trans('sigac::attendance.Btn_Register_Previous') }}
                                         </button>
                                     </div>
                                 </div>
@@ -78,7 +78,7 @@
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">
-                        {{ trans('sigac::attendance.Summary of the session') }}
+                        {{ trans('sigac::attendance.Title_Summary_Session') }}
                     </h3>
                 </div>
                 <div class="card-body">
@@ -86,21 +86,21 @@
                         <div class="col-6">
                             <dl>
                                 <dt>P</dt>
-                                <dd>{{ trans('sigac::attendance.Total present:') }} 0</dd>
+                                <dd>{{ trans('sigac::attendance.Text_Total_Present') }} 0</dd>
                             </dl>
                             <dl>
                                 <dt>FJ</dt>
-                                <dd>{{ trans('sigac::attendance.Total justified failures:') }} 0</dd>
+                                <dd>{{ trans('sigac::attendance.Text_Total_Justified_Failures') }} 0</dd>
                             </dl>
                         </div>
                         <div class="col-6">
                             <dl>
                                 <dt>FI</dt>
-                                <dd>{{ trans('sigac::attendance.Total unjustified failures:') }} 0</dd>
+                                <dd>{{ trans('sigac::attendance.Text_Total_Unjustified_Failures') }} 0</dd>
                             </dl>
                             <dl>
                                 <dt>MD</dt>
-                                <dd>{{ trans('sigac::attendance.Total missing stockings:') }} 0</dd>
+                                <dd>{{ trans('sigac::attendance.Text_Total_Missing_Stockings') }} 0</dd>
                             </dl>
                         </div>
                     </div>
@@ -116,7 +116,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">
-                        {{ trans('sigac::attendance.CardSubtitle2') }}</h1>
+                        {{ trans('sigac::attendance.Title_Modal_Register_Previous') }}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -129,8 +129,8 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-dark"
-                        data-bs-dismiss="modal">{{ trans('sigac::attendance.Close') }}</button>
-                    <button type="button" class="btn btn-success">{{ trans('sigac::attendance.Accept') }}</button>
+                        data-bs-dismiss="modal">{{ trans('sigac::attendance.Btn_Close') }}</button>
+                    <button type="button" class="btn btn-success">{{ trans('sigac::attendance.Btn_Accept') }}</button>
                 </div>
             </div>
         </div>
@@ -141,17 +141,17 @@
             <div class="card">
                 <div class="card-body">
                     @if (empty($apprentices))
-                        <div class="text-center">No se ha selecionado un curso</div>
+                        <div class="text-center">{{ trans('sigac::attendance.E1_Text_No_Results') }}</div>
                     @else
                         <div class="table-responsive">
                             <table class="table table-striped" id="tableAttendance">
                                 <thead class="table-dark">
                                     <tr>
-                                        <th scope="col">{{ trans('sigac::attendance.Code') }}</th>
-                                        <th scope="col">{{ trans('sigac::attendance.Name') }}</th>
-                                        <th scope="col">{{ trans('sigac::attendance.Document Number') }}</th>
-                                        <th scope="col">{{ trans('sigac::attendance.Accumulated Faults') }}</th>
-                                        <th scope="col">{{ trans('sigac::attendance.Attendance') }}</th>
+                                        <th scope="col">{{ trans('sigac::attendance.1T1_Code') }}</th>
+                                        <th scope="col">{{ trans('sigac::attendance.1T2_Name') }}</th>
+                                        <th scope="col">{{ trans('sigac::attendance.1T3_Document_Number') }}</th>
+                                        <th scope="col">{{ trans('sigac::attendance.1T4_Accumulated_Faults') }}</th>
+                                        <th scope="col">{{ trans('sigac::attendance.1T5_Attendance') }}</th>
                                     </tr>
                                 </thead>
                                 <tbody>
