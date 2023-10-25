5f7d4ec94ef35b36ba1a8cc5dbb5bb7051e2edaa
Kevinvaldes77 <santiagovaldez1214@gmail.com>
2023-10-25 10:39:57 -0500

    BIENESTAR - Agregacion De Busqueda Vista De Asistencia De Transporte

--- Modules/BIENESTAR/Resources/views/transportation_assistance_list.blade.php
+++ Modules/BIENESTAR/Resources/views/transportation_assistance_list.blade.php
@@ -1,6 +1,25 @@
 @extends('bienestar::layouts.master')
 
-@section('content')
-    <!-- El contenido de tu vista va aquí -->
-    <h1>Asistencia De Transporte</h1>
+@section('content') 
+
+    <h1>Asistencia De Transporte</h1> <h1><i class="fas fa-bus"></i></h1>
+
+    <div class="container-fluid" style="max-width: 1200px;">
+        <div class="row justify-content-md-center pt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form class="form-inline d-flex align-items-center justify-content-center">
+                            <div class="form-group">
+                                <input type="number" class="form-control" id="search" placeholder="Buscar" pattern="[0-9]*">
+                            </div>
+                            <button type="submit" class="btn btn-primary ml-2">Buscar</button>
+                        </form>
+                    </div>
+                </div>
+                <!-- Otro contenido de la vista va aquí -->
+            </div>
+        </div>
+    </div>
+    
 @endsection
