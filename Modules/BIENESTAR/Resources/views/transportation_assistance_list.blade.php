cfbdcf1f3bc273ec792afa0c3723045d6ea06d64
Mauroreyes04 <carlosmauriciopolancoreyes@gmail.com>
2023-10-25 10:29:12 -0500

    BIENESTRA - tabla

--- Modules/BIENESTAR/Resources/views/transportation_assistance_list.blade.php
+++ Modules/BIENESTAR/Resources/views/transportation_assistance_list.blade.php
@@ -1,6 +1,30 @@
 @extends('bienestar::layouts.master')
 
 @section('content')
-    <!-- El contenido de tu vista va aquí -->
-    <h1>Asistencia De Transporte</h1>
+    <h1>Registro de Asistencias</h1>
+    
+    <div class="container-fluid">
+    <div class="row justify-content-md-center pt-4">
+        <div class="card shadow col-md-10">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Fecha</th>
+                    <th>Nombre del Asistente</th>
+                    <th>Asistencia</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                
+            </tbody>
+        </table>
+        </div>
+        </div>
+    </div>
 @endsection
