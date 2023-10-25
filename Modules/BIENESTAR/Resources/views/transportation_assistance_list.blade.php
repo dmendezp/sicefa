0e11911d33e551d135ac8ecab72aa9d57007b92d
Mauroreyes04 <carlosmauriciopolancoreyes@gmail.com>
2023-10-25 11:20:34 -0500

    BIENESTAR - creacion de tabla, agregue boton de pdf y excel

--- Modules/BIENESTAR/Resources/views/transportation_assistance_list.blade.php
+++ Modules/BIENESTAR/Resources/views/transportation_assistance_list.blade.php
@@ -2,22 +2,40 @@
 
 @section('content') 
 
-    <h1>Asistencia De Transporte</h1> <h1><i class="fas fa-bus"></i></h1>
+    <h1>Asistencia De Transporte <i class="fas fa-bus"></i></h1>
 
     <div class="container-fluid" style="max-width: 1200px;">
         <div class="row justify-content-md-center pt-4">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        <form class="form-inline d-flex align-items-center justify-content-center">
-                            <div class="form-group">
-                                <input type="number" class="form-control" id="search" placeholder="Buscar" pattern="[0-9]*">
-                            </div>
-                            <button type="submit" class="btn btn-primary ml-2">Buscar</button>
-                        </form>
+                        <!-- Tabla de Asistencias -->
+                        <div class="container mt-4">
+                            <table id="datatable" class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                    <th>id</th>
+                                        <th>Aprendiz</th>
+                                        <th>ruta de transporte</th>
+                                        <th>Convocatoria</th>
+                                        <th>acciones</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <!-- Puedes iterar sobre los datos reales aquí -->
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                <!-- Otro contenido de la vista va aquí -->
             </div>
         </div>
     </div>
