c8661adbe3dbd1670274ac5ca337d4c831117bb9
Davidjr16 <davidrojasjr16@gmail.com>
2023-11-08 20:51:12 -0500

    HDC - Vista de desarrolladores Completada Con sus traducciones

--- Modules/HDC/Resources/lang/en/developers.php
+++ Modules/HDC/Resources/lang/en/developers.php
@@ -3,7 +3,4 @@
 return [
     // Indicador de la vista 
     'developers' => 'Developers',
-    
-    // Indicador de pagina desarrolladores
-    'Indicator_developers' => 'Indicator_Developers',
 ];
--- Modules/HDC/Resources/lang/es/developers.php
+++ Modules/HDC/Resources/lang/es/developers.php
@@ -3,7 +3,4 @@
 return [
     // Indicador de la vista 
     'developers' => 'Desarrolladores',
-    
-    // Indicador de pagina desarrolladores
-    'Indicator_developers' => 'Desarrolladores',
 ];
