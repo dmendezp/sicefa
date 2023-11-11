4d0a063c808d1d4367bbc3cacc77ec228002cd68
Julianchiki <julianjavierramirezdiaz73@gmail.com>
2023-11-11 16:44:28 -0500

    AGROINDUSTRIA - Cambiar estado de solicitud y enviar produccion a punto de venta

--- Modules/AGROINDUSTRIA/Resources/lang/en/request.php
+++ Modules/AGROINDUSTRIA/Resources/lang/en/request.php
@@ -3,35 +3,18 @@
     return [
         //table
         'date' => 'Date',
-        'coordinator' => 'Coordinator',
-        'receiver' => 'Receiver',
-        'course' => 'Course',
-        'status' => 'Status',
-
-        //form
-        'requestSupplies' => 'Request for supplies',
-        'dateRequest' => 'Date of request',
-        'nameOfficeHeadAreaCoordinator' => 'Name of office head or area coordinator',
-        'documentNumber' => 'Document number',
-        'namePersonWhomPropertyWillAssigned' => 'Name of the person to whom the property will be assigned',
-        'groupCodeCharacterizationSheet' => 'Group code or characterization sheet',
-        'products' => 'Products',
-        'SENACode' => 'SENA Code',
-        'amount' => 'Amount',
-        'observations' => 'Observations',
+        'element' => 'Element',
+        'applicant' => 'Applicant',
 
         //botones
-        'addProduct' => 'Add Product',
-        'delete' => 'Delete',
-        'send' => 'Send',
-
-        //modal
-        'cancelRequest' => 'Cancel Request',
-        'doYouWantCancelThisRequest' => 'Do you want to cancel this request?',
+        'approve' => 'Approve',
 
         //alerts
-        'applicationCancelled' => 'Application cancelled',
-        'iCannotCancelRequest' => 'I cannot cancel the request',
+        'areYouSure' => 'Are you sure?',
+        'thisActionWillMarkRequestApproved.' => 'This action will mark the request as approved.',
+        'yesApprove' => 'Yes, approve',
+        'approved' => 'Approved',
+        'theRequestBeenMarkedApproved' => 'The request has been marked as approved.',
     ]
 
 ?>
