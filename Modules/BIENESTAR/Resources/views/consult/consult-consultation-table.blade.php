95bfe68700b90717608c9b3377ba7d34dd222621
BrayanDavidL <brayangarzon474@gmil.com>
2023-12-08 22:14:30 -0500

    BIENESTAR - Cambio de frontend de la vista de consulta

--- Modules/BIENESTAR/Resources/views/consult/consult-consultation-table.blade.php
+++ Modules/BIENESTAR/Resources/views/consult/consult-consultation-table.blade.php
@@ -1,1 +1,40 @@
-<h1>{{ $documentNumber }}</h1>
+<div class="row justify-content-md-center pt-4">
+    <div class="card shadow col-md-14">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Documento</th>
+                            <th>Nombre</th>
+                            <th>Primer Apellido</th>
+                            <th>Segundo Apellido</th>
+                            <th>Código del Curso</th>
+                            <th>Programa</th>
+                            <th>Estado</th>
+                            <th>Beneficio</th>
+                            <th>Porcentaje</th>
+                            <th>Mensaje</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($result as $item)
+                        <tr>
+                            <td>{{ $item->document_number }}</td>
+                            <td>{{ $item->first_name }}</td>
+                            <td>{{ $item->first_last_name }}</td>
+                            <td>{{ $item->second_last_name }}</td>
+                            <td>{{ $item->code }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->state }}</td>
+                            <td>{{ $item->benefit_name }}</td>
+                            <td>{{ $item->porcentege }}</td>
+                            <td>{{ $item->message }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
