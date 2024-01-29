ff705c1f8e314b0e7b5b94f09f0ed1c41b64b649
diego222709 <diegopenagos955@gmail.com>
2024-01-28 22:06:08 -0500

    SENAEMPRESA - arreglo

--- Modules/SENAEMPRESA/Resources/views/Company/manual/manual_psychologo.blade.php
+++ Modules/SENAEMPRESA/Resources/views/Company/manual/manual_psychologo.blade.php
@@ -1,9 +1,109 @@
 @extends('senaempresa::layouts.master')
+
 @section('content')
-    <p>
-        <span>
-            Manual del psicologo
-        </span>
-        &mdash; JSM6580 &mdash;
-    </p>
+    <div class="container">
+        <h1><a href="https://app.tango.us/app/workflow/2a660247-1221-4c4c-abe2-1687c600bd43?utm_source=magicCopy&utm_medium=magicCopy&utm_campaign=workflow%20export%20links"
+                target='_blank'>manual de usuario psicologo</a></h1>
+        <div><b>Creation Date:</b> January 28, 2024</div>
+        <div><a href="https://app.tango.us/app/workflow/2a660247-1221-4c4c-abe2-1687c600bd43?utm_source=magicCopy&utm_medium=magicCopy&utm_campaign=workflow%20export%20links"
+                target='_blank'>View most recent version on Tango.us</a></div>
+        <div style="height: 24px">&#8203;</div>
+        <hr />
+        <div style="height: 24px">&#8203;</div>
+
+
+        <div>
+            <h2><a href="http://127.0.0.1:8000/senaempresa/psychologo"># SENA EMPRESA | Psicologo</a></h2>
+        </div>
+
+        <div>
+            <h3>1. Hacer clic en "Psicólogo"</h3>
+            <p>Al acceder a esta sección, podrás visualizar las opciones disponibles para los psicólogos.</p>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/bf1ba81d-b548-45dd-9787-3d100e924027/e1e9b710-6fda-449e-aa65-1685cf77241a.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.5000&fp-y=0.5000&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=396&mark-y=5&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTQlMkNGRjc0NDImdz0xMTImaD01MSZmaXQ9Y3JvcCZjb3JuZXItcmFkaXVzPTEw"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="Hacer clic en &quot;Psicólogo&quot;" />
+        </div>
+
+        <div>
+            <h3>2. Hacer clic en "Postulados"</h3>
+            <p>En esta sección, podrás asignar puntuaciones a los aprendices que se han postulado para las vacantes.</p>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/dbefb424-2294-4338-a933-17a46759ccc5/4671cf1a-3d00-406a-a2fa-5f90c4f6dfd4.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.5000&fp-y=0.5000&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=5&mark-y=318&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTQlMkNGRjc0NDImdz0yNjAmaD01MSZmaXQ9Y3JvcCZjb3JuZXItcmFkaXVzPTEw"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="Hacer clic en &quot;Postulados&quot;" />
+        </div>
+
+        <div>
+            <h3>3. Información básica del postulado y la vacante</h3>
+            <p> datos esenciales sobre el postulado y la vacante.</p>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/62ee49f7-8085-45a4-a642-720d8514068f/ab0f5a41-b7ba-4a20-8db6-f5e790c55aef.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.6050&fp-y=0.6244&fp-z=1.3364&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=34&mark-y=225&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTYlMkNGRjc0NDImdz0xMTMzJmg9MzMyJmZpdD1jcm9wJmNvcm5lci1yYWRpdXM9MTA%3D"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="Información básica del postulado y la vacante" />
+        </div>
+
+        <div>
+            <h3>4. Hacer clic en "PDF"</h3>
+            <p>Aquí puedes descargar el PDF al cual asignarás una puntuación</p>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/a4945ebf-a6b7-4486-b896-cce19bb17709/26affc33-72ca-4bdb-af8a-0e10d26c0e37.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.5000&fp-y=0.5000&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=781&mark-y=472&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTQlMkNGRjc0NDImdz0zMyZoPTMwJmZpdD1jcm9wJmNvcm5lci1yYWRpdXM9MTA%3D"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600" alt="Hacer clic en &quot;PDF&quot;" />
+        </div>
+
+        <div>
+            <h3>5. Hacer clic en "Asignar"</h3>
+            <p>Al presionar este botón, serás redirigido a una nueva vista para asignar puntuaciones.</p>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/fa50e27f-73cb-445e-97d9-a4f7ad7b9841/dbf50a90-b5a1-457e-9685-b25771ca85cf.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.5000&fp-y=0.5000&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=1027&mark-y=460&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTQlMkNGRjc0NDImdz03NSZoPTQyJmZpdD1jcm9wJmNvcm5lci1yYWRpdXM9MTA%3D"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="Hacer clic en &quot;Asignar&quot;" />
+        </div>
+
+        <div>
+            <h3>6. Asignar puntuación</h3>
+            <p>En esta vista, visualizarás el nombre, vacante y las dos puntuaciones que debes asignar.</p>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/d5946bb7-e720-4c7a-8e75-ce7f035a0cc3/50c537f7-ddac-4c05-ab81-0dc0d70273eb.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.6114&fp-y=0.5763&fp-z=1.7819&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=231&mark-y=347&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTYlMkNGRjc0NDImdz03MzgmaD04NyZmaXQ9Y3JvcCZjb3JuZXItcmFkaXVzPTEw"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600" alt="Asignar puntuación" />
+        </div>
+
+        <div>
+            <h3>7. La puntuación no puede exceder 100</h3>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/7de9d294-3526-46a2-bc46-2f55307e40f2/56a2b30e-bc0a-4150-bc3d-c033ccf31961.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.6114&fp-y=0.6954&fp-z=1.7819&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=231&mark-y=347&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTYlMkNGRjc0NDImdz03MzgmaD04NyZmaXQ9Y3JvcCZjb3JuZXItcmFkaXVzPTEw"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="La puntuación no puede exceder 100" />
+        </div>
+
+        <div>
+            <h3>8. Notificación: Puntuación agregada con éxito</h3>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/7b1064f9-2c46-4008-839b-237c9cb0dd8e/05eac40b-701b-44ad-bbfc-106cd526bef3.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.5000&fp-y=0.5000&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=527&mark-y=577&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTQlMkNGRjc0NDImdz05MSZoPTQ5JmZpdD1jcm9wJmNvcm5lci1yYWRpdXM9MTA%3D"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="Notificación: Puntuación agregada con éxito" />
+        </div>
+
+        <div>
+            <h3>9. alerta de que no puedes escribir un numero mayor a 100</h3>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/d64c43a6-1703-4dc6-9950-93fc8d459568/d3bf46f5-7d87-4bbd-bb0b-8823f12ac1c3.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.5000&fp-y=0.5000&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=527&mark-y=485&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTQlMkNGRjc0NDImdz00MTQmaD04NCZmaXQ9Y3JvcCZjb3JuZXItcmFkaXVzPTEw"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="alerta de que no puedes escribir un numero mayor a 100" />
+        </div>
+
+        <div>
+            <h3>10. notificación de puntaje agregado con éxito</h3>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/c58eda48-fb40-4888-9aac-8e2d4e3e025f/2b16d0f6-db70-424d-bb7d-d2b09701608f.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.4995&fp-y=0.4992&fp-z=1.3636&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=219&mark-y=188&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTYlMkNGRjc0NDImdz03NjImaD00MDcmZml0PWNyb3AmY29ybmVyLXJhZGl1cz0xMA%3D%3D"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="notificación de puntaje agregado con éxito" />
+        </div>
+
+        <div>
+            <h3>11. Ver a quién ya se le ha asignado puntuación</h3>
+            <img src="https://images.tango.us/workflows/2a660247-1221-4c4c-abe2-1687c600bd43/steps/579a8546-e6c0-46ec-9ca3-26451de25082/e0986d9a-ae61-46b1-b7c6-8d11edba3233.png?fm=png&crop=focalpoint&fit=crop&fp-x=0.6050&fp-y=0.6351&fp-z=1.3364&w=1200&border=2%2CF4F2F7&border-radius=8%2C8%2C8%2C8&border-radius-inner=8%2C8%2C8%2C8&blend-align=bottom&blend-mode=normal&blend-x=0&blend-w=1200&blend64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmstdjIucG5n&mark-x=34&mark-y=224&m64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL2JsYW5rLnBuZz9tYXNrPWNvcm5lcnMmYm9yZGVyPTYlMkNGRjc0NDImdz0xMTMzJmg9MzU0JmZpdD1jcm9wJmNvcm5lci1yYWRpdXM9MTA%3D"
+                style="border-radius: 8px; border: 1px solid #F4F2F7;" width="600"
+                alt="Ver a quién ya se le ha asignado puntuación" />
+        </div>
+
+        <br />
+        <hr />
+        <div>
+            <span>Created with </span><a
+                href="https://tango.us?utm_source=magicCopy&utm_medium=magicCopy&utm_campaign=workflow%20export%20links"
+                target='_blank' style='color: #256EFF'>Tango.us
+            </a>
+        </div>
+    </div>
 @endsection
