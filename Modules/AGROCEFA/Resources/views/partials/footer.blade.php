bbcc074f1417f87550002dcb99087feffefb64b0
Afandres <andresfelipealmario2309@gmail.com>
2024-02-06 12:40:13 -0500

    AGROCEFA - Ajustes de frontend

--- Modules/AGROCEFA/Resources/views/partials/footer.blade.php
+++ Modules/AGROCEFA/Resources/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <!--Grid column-->
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">AGROCEFA <img src="{{ asset('agrocefa/images/usuario/tractor.png') }}"
+                <h5 class="text-uppercase">AGROCEFA <img src="{{ asset('modules/agrocefa/images/usuario/tractor.png') }}"
                         class="logo">
                 </h5>
 
@@ -15,44 +15,26 @@
             <!--Grid column-->
 
             <!--Grid column-->
-<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-    <h5 class="text-uppercase">{{trans('agrocefa::footer.SOCIAL NETWORKS')}}</h5>
-<br>
-    <ul class="list-unstyled mb-0">
-        <li>
-            <a href="enlace-a-tu-perfil-de-Facebook" class="text-white">
-                <i class="fab fa-facebook"></i> Facebook
-            </a>
-        </li>
-        <li>
-            <a href="enlace-a-tu-perfil-de-Twitter" class="text-white">
-                <i class="fab fa-twitter"></i> Twitter
-            </a>
-        </li>
-        <!-- Agrega más iconos y enlaces para otras redes sociales según sea necesario -->
-    </ul>
-</div>
-<!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0 networks">
+                <h5 class="text-uppercase">{{trans('agrocefa::footer.SOCIAL NETWORKS')}}</h5>
+            <br>
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        <a href="https://web.facebook.com/profile.php?id=100014602595454&locale=es_LA" class="text-white">
+                            <i class="fab fa-facebook"></i> Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/sena.empresa/" class="text-white">
+                            <i class="fab fa-instagram"></i> Instagram
+                        </a>
+                    </li>
+                    <!-- Agrega más iconos y enlaces para otras redes sociales según sea necesario -->
+                </ul>
+            </div>
+            <!--Grid column-->
 
-<!--Grid column-->
-<div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-    <h5 class="text-uppercase mb-0">{{trans('agrocefa::footer.MORE SOCIAL NETWORKS')}}</h5>
-<br>
-    <ul class="list-unstyled">
-        <li>
-            <a href="enlace-a-tu-perfil-de-Instagram" class="text-white">
-                <i class="fab fa-instagram"></i> Instagram
-            </a>
-        </li>
-        <li>
-            <a href="enlace-a-tu-perfil-de-LinkedIn" class="text-white">
-                <i class="fab fa-linkedin"></i> LinkedIn
-            </a>
-        </li>
-        <!-- Agrega más iconos y enlaces para otras redes sociales según sea necesario -->
-    </ul>
-</div>
-<!--Grid column-->
+
 
         </div>
         <!--Grid row-->
@@ -61,8 +43,8 @@
 
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2023 Copyright:
-        <a class="text-white font-italic" href="#">AGROCEFA</a>
+        © 2023 Copyright: Fábrica de Software. Todos los derechos reservados.
+        
     </div>
     <!-- Copyright -->
 </footer>
