14c1d2c3fab3ba6a11f91b662d97fd2fe4b274b6
Sofia032 <osorioleidysofia@gmail.com>
2024-02-07 14:13:53 -0500

    se expandio el formulario horizontalmente

--- Modules/HANGARAUTO/Resources/views/solicitar.blade.php
+++ Modules/HANGARAUTO/Resources/views/solicitar.blade.php
@@ -3,103 +3,61 @@
 @push('breadcrumbs')
     <li class="breadcrumb-item active">{{ trans('hangarauto::solicitar.Request_Vehicle') }}</li>
 @endpush
+
 @section('content')
-    {!! Html::script('js/jquery-2.1.0.min.js') !!}
-    {!! Html::script('js/dropdown.js') !!}
     <section class="content-header">
         <div class="content">
             <div class="row justify-content-center">
-                <div class="card card-primary card-outline shadow col-md-4">
+                <div class="card card-primary card-outline shadow col-md-8">
                     <div class="card-header">
-                        <h3>Solicitar Vehiculo</h3>
+                        <h3>{{ trans('hangarauto::solicitar.Request_Vehicle') }}</h3>
                     </div>
-                    <br>
-                    <div class="form_search" id="form_search">
-                        {!! Form::open(['url' => route('cefa.parking.solicitar.search')]) !!}
+                    <div class="card-body">
+                        {!! Form::open(['url' => route('cefa.parking.guardar'), 'id' => 'vehicle-request-form']) !!}
                         <div class="row">
-                            <div class="col-md-8">
-                                {!! Form::text('search', null, ['class' => 'form-control', 'placeholder' => 'Ingrese N° De Documento', 'required']) !!}
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    {!! Form::label('search', trans('Nombre')) !!}
+                                    {!! Form::text('search', null, ['class' => 'form-control', 'placeholder' => trans('Nombre'), 'required']) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('start_date', trans('FechaInicio')) !!}
+                                    {!! Form::date('start_date', null, ['class' => 'form-control']) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('end_date', trans('Fecha fin')) !!}
+                                    {!! Form::date('end_date', null, ['class' => 'form-control']) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('department', trans('Departamento Donde Se Dirige')) !!}
+                                    {!! Form::select('department', $department, null, ['class' => 'form-control', 'placeholder' => trans('Seleccione El Departamento'), 'id' => 'department']) !!}
+                                </div>
                             </div>
-                            <div class="col-md-4">
-                                {!! Form::submit('Buscar', ['class' => 'btn btn-success']) !!}
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div id="divMunicipality">
+                                        <!-- Municipality field will be populated by AJAX -->
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('department', trans('Cuidad Donde Se Dirige')) !!}
+                                    {!! Form::select('department', $department, null, ['class' => 'form-control', 'placeholder' => trans('Selecciona La Cuidad'), 'id' => 'department']) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('numstudents', trans('Numero De Pasajeros')) !!}
+                                    {!! Form::number('numstudents', null, ['class' => 'form-control']) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::label('reason', trans('Motivo De Viaje')) !!}
+                                    {!! Form::textarea('reason', null, ['class' => 'form-control']) !!}
+                                </div>
                             </div>
+                        </div>
+                        <div class="form-group">
+                            {!! Form::submit(trans('ENVIAR'), ['class' => 'btn btn-success']) !!}
                         </div>
                         {!! Form::close() !!}
                     </div>
-                    {!! Form::open(['url' => route('cefa.parking.guardar')]) !!}
-                    @if(@isset($people))
-                        @if(is_null($people))
-                            <h1>"Documento No Encontrado"</h1>
-                            @else
-                                <label class="mtop16" for="name">Nombre:</label>
-                                <div>
-                                    {{ $people->first_name." ".$people->first_name." ".$people->second_last_name }}
-                                    {!! Form::hidden('person_id', $people->id) !!}
-                                </div>
-                        @endif
-                    @endif
-                    <label for="name">Fecha De Inicio:</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">
-                                <i class="fa-solid fa-calendar-days"></i>
-                                {{ Form::date('dataTime-local', 'start_date', null, ['id' => 'game-date-time-text', 'class' => 'form-control']) }}
-                            </span>
-                        </div>
-                    </div>
-
-                    <label for="name">Fecha De Fin:</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">
-                                <i class="fa-solid fa-calendar-days"></i>
-                                {{ Form::date('dateTime-local', 'end_date', null, ['id' => 'game-date-time-text', 'class' => 'form-control']) }}
-                            </span>
-                        </div>
-                    </div>
-
-                    <label for="name">Departamento Donde Se Dirije:</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">
-                                <i class="fa-solid fa-angle-down"></i>
-                            </span>
-                        </div>
-                        {!! Form::select('department',$department, null, ['class' => 'form-control', 'placeholder' => '-- Seleccione --', 'id' => 'department']) !!}
-                    </div>
-                    <div id="divMunicipality">
-                        <label for="name">Ciudad Donde Se Dirige:</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1">
-                                    <i class="fa-solid fa-angle-down"></i>
-                                </span>
-                            </div>
-                            {!! Form::select('municipality',[], null, ['class' => 'form-control', 'placeholder' => '-- Seleccione --', 'id' => 'municipality']) !!}
-                        </div>
-                    </div>
-                    <label for="name">N° De Pasajeros:</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">
-                                <i class="far fa-keyboard"></i>
-                            </span>
-                        </div>
-                        {!! Form::number('numstudents', null, ['class' => 'form-control']) !!}
-                    </div>
-                    <label for="name">Motivo Del Viaje:</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">
-                                <i class="far fa-keyboard"></i>
-                            </span>
-                        </div>
-                        {!! Form::textarea('reason', null, ['class' => 'form-control']) !!}
-                    </div>
-                    <div class="mt-4 text-center mb-4">
-                        {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
-                    </div>
-                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
@@ -107,21 +65,42 @@
 @stop
 
 @section('js')
-    <script type="text/javascript">
+    <script>
         $(document).ready(function() {
             $("#department").change(function() {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
                 $.ajax({
                     method: "POST",
-                    url: "{{route('cefa.parking.solicitar.municipios.search')}}",
-                    data: {department_id: $(this).val()}
-                })
-                .done(function(html){
-                    $("#divMunicipality").html(html);
+                    url: "{{ route('cefa.parking.solicitar.municipios.search') }}",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: {department_id: $(this).val()},
+                    success: function(html) {
+                        $("#divMunicipality").html(html);
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(xhr.responseText);
+                    }
+                });
+            });
+
+            $('#vehicle-request-form').submit(function(event) {
+                event.preventDefault();
+                var formData = $(this).serialize();
+                $.ajax({
+                    method: "POST",
+                    url: $(this).attr('action'),
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: formData,
+                    success: function(response) {
+                        // Handle success response
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(xhr.responseText);
+                        // Handle error response
+                    }
                 });
             });
         });
