384a5de848e09cb8847b66379594e5f9b43bbe85
Afandres <andresfelipealmario2309@gmail.com>
2024-03-13 14:02:04 -0500

    FABRICA5|AGROCEFA - Modificacion en el diseño del navbar y sidebar

--- public/modules/agrocefa/js/sidebarclose.js
+++ public/modules/agrocefa/js/sidebarclose.js
@@ -9,6 +9,14 @@ toggle.addEventListener("click" , () => {
   sidebar.classList.toggle("close");
 });
 
+sidebar.addEventListener("mouseenter" , () => {
+  sidebar.classList.remove("close"); // Remueve la clase "close" para expandir el sidebar
+});
+
+sidebar.addEventListener("mouseleave", () => {
+  sidebar.classList.add("close"); // Agrega la clase "close" para cerrar el sidebar
+});
+
 // Cierra el sidebar al hacer clic en el elemento con clase "home"
 home.addEventListener("click" , () => {
   sidebar.classList.add("close");
