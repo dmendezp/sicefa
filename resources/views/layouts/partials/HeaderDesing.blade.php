adea92dd9bea6a5ef27f9dfac481d973e5db5694
Afandres <andresfelipealmario2309@gmail.com>
2024-03-13 11:04:20 -0500

    FABRICA5 - Navbar responsive de sicefa y modificacion en productos de venta

--- resources/views/layouts/partials/HeaderDesing.blade.php
+++ resources/views/layouts/partials/HeaderDesing.blade.php
@@ -1,48 +1,82 @@
 <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top"style="background-color: rgb(249, 89, 40, 0.7); ">
-    <div class="container d-flex align-items-center">
+<link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+<header id="header" class="fixed-top"style="background-color: rgb(249, 89, 40, 0.7); ">
+  <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="{{ route('cefa.welcome') }}">SICEFA</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto" href="{{ route('cefa.welcome') }}">Inicio</a></li>
-          <li><a class="nav-link scrollto" href="{{ route('cefa.developers') }}">Desarrolladores</a></li>
-          <li class="dropdown">
+    <h1 class="logo me-auto"><a href="{{ route('cefa.welcome') }}">SICEFA</a></h1>
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+    <nav id="navbar-top" class="navbar" style="margin-right: 20px">
+      <div class="navbar-nav">
+          <div class="dropdown d-lg-none">
+            <li><a class="nav-link scrollto" href="{{ route('cefa.welcome') }}">Inicio</a></li>
+          </div>
+          <div class="dropdown developers d-lg-none">
+            <li><a class="nav-link scrollto" href="{{ route('cefa.developers') }}">Desarrolladores</a></li>
+          </div>
+          <div class="dropdown d-lg-none">
               @auth
-                 <a href="{{ route('cefa.home') }}">{{ Auth::user()->nickname }}</a>
-                <ul>
-                  <li><a href="{{ route('password.change.index') }}">Cambiar contraseña</a></li>
-                  <li>
-                    
-                <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">Salir</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              <a href="{{ route('cefa.home') }}">{{ Auth::user()->nickname }}</a>
+              <div class="dropdown-menu">
+                  <a href="{{ route('password.change.index') }}">Cambiar contraseña</a>
+                  <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">Salir</a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                       @csrf
-                </form>
-
-                  </li>
-                </ul>              
+                  </form>
+              </div>
               @else
-                  <a href="{{ route('login') }}">Log in</a>
-              @endauth            
+              <a href="{{ route('login') }}">Log in</a>
+              @endauth
+          </div>
+          <div class="dropdown lang d-lg-none">
+              <a class="nav-link scrollto" data-toggle="dropdown" href="#">
+                  {{ session('lang') }} <i class="fas fa-globe"></i>
+              </a>
+              <div class="dropdown-menu">
+                  <a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a>
+                  <a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a>
+              </div>
+          </div>
+      </div>
+  </nav>
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a class="nav-link scrollto" href="{{ route('cefa.welcome') }}">Inicio</a></li>
+        <li><a class="nav-link scrollto" href="{{ route('cefa.developers') }}">Desarrolladores</a></li>
+        <li class="dropdown">
+            @auth
+                <a href="{{ route('cefa.home') }}">{{ Auth::user()->nickname }}</a>
+              <ul>
+                <li><a href="{{ route('password.change.index') }}">Cambiar contraseña</a></li>
+                <li>
+                  
+              <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">Salir</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+              </form>
 
-          </li>
-          <li class="dropdown">
-            <a class="nav-link scrollto" data-toggle="dropdown" href="#">
-              {{ session('lang') }} <i class="fas fa-globe"></i>
-            </a>
-            <ul>
-              <li><a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a></li>
-              <li><a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a></li>
-            </ul>
-          </li>
-        </ul>
+                </li>
+              </ul>              
+            @else
+                <a href="{{ route('login') }}">Log in</a>
+            @endauth            
 
-      </nav><!-- .navbar -->
+        </li>
+        <li class="dropdown">
+          <a class="nav-link scrollto" data-toggle="dropdown" href="#">
+            {{ session('lang') }} <i class="fas fa-globe"></i>
+          </a>
+          <ul>
+            <li><a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a></li>
+            <li><a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a></li>
+          </ul>
+        </li>
+      </ul>
 
-    </div>
+    </nav><!-- .navbar -->
 
-  </header><!-- End Header -->
+  </div>
+
+</header><!-- End Header -->
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,98 +1,97 @@
 <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top"style="background-color: rgb(249, 89, 40, 0.7); ">
-    <div class="container d-flex align-items-center">
+<link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+<header id="header" class="fixed-top"style="background-color: rgb(249, 89, 40, 0.7); ">
+  <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="{{ route('cefa.welcome') }}">SICEFA</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      <nav id="navbar" class="navbar" style="margin-right: 20px">
-          <li class="dropdown d-lg-none">
-            <a class="nav-link scrollto" data-toggle="dropdown" href="#">
-              Menu
-            </a>
-            <ul>
-              <a class="nav-link scrollto  active" href="#hero">Inicio</a>
-              <a class="nav-link scrollto" href="#ptoventa">Punto de venta</a>
-              <a class="nav-link scrollto" href="#modules">Aplicaciones</a>
-              <a class="nav-link scrollto" href="#cefa">Map</a>
-              <a class="nav-link scrollto" href="#why-us">SENA-Empresa</a>
-              <a class="nav-link scrollto" href="#about">Acerca</a>
-              <a class="nav-link scrollto" href="#contact">PQRS</a>
-            </ul>
-          </li>
-          <li class="dropdown d-lg-none">
+    <h1 class="logo me-auto"><a href="{{ route('cefa.welcome') }}">SICEFA</a></h1>
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+    <nav id="navbar-top" class="navbar" style="margin-right: 20px">
+      <div class="navbar-nav">
+          <div class="dropdown d-lg-none">
+              <a class="nav-link scrollto" data-toggle="dropdown" href="#">
+                  Menu
+              </a>
+              <div class="dropdown-menu">
+                  <a class="nav-link scrollto active" href="#hero">Inicio</a>
+                  <a class="nav-link scrollto" href="#ptoventa">Punto de venta</a>
+                  <a class="nav-link scrollto" href="#modules">Aplicaciones</a>
+                  <a class="nav-link scrollto" href="#cefa">Map</a>
+                  <a class="nav-link scrollto" href="#why-us">SENA-Empresa</a>
+                  <a class="nav-link scrollto" href="#about">Acerca</a>
+                  <a class="nav-link scrollto" href="#contact">PQRS</a>
+              </div>
+          </div>
+          <div class="dropdown d-lg-none">
               @auth
+              <a href="{{ route('cefa.home') }}">{{ Auth::user()->nickname }}</a>
+              <div class="dropdown-menu">
+                  <a href="{{ route('password.change.index') }}">Cambiar contraseña</a>
+                  <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">Salir</a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                      @csrf
+                  </form>
+              </div>
+              @else
+              <a href="{{ route('login') }}">Log in</a>
+              @endauth
+          </div>
+          <div class="dropdown lang d-lg-none">
+              <a class="nav-link scrollto" data-toggle="dropdown" href="#">
+                  {{ session('lang') }} <i class="fas fa-globe"></i>
+              </a>
+              <div class="dropdown-menu">
+                  <a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a>
+                  <a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a>
+              </div>
+          </div>
+      </div>
+  </nav>
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
+        <li><a class="nav-link scrollto" href="#ptoventa">Punto de venta</a></li>
+        <li><a class="nav-link scrollto" href="#modules">Aplicaciones</a></li>
+        <li><a class="nav-link scrollto" href="#cefa">Map</a></li>
+        <li><a class="nav-link scrollto" href="#why-us">SENA-Empresa</a></li>
+        <li><a class="nav-link scrollto" href="#about">Acerca</a></li>
+        <li><a class="nav-link scrollto" href="#contact">PQRS</a></li>
+        <li class="dropdown">
+            @auth
                 <a href="{{ route('cefa.home') }}">{{ Auth::user()->nickname }}</a>
-                <ul>
-                  <li><a href="{{ route('password.change.index') }}">Cambiar contraseña</a></li>
-                  <li>
+              <ul>
+                <li><a href="{{ route('password.change.index') }}">Cambiar contraseña</a></li>
+                <li>
 
-                <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">Salir</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                      @csrf
-                </form>
+              <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">Salir</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+              </form>
 
-                  </li>
-                </ul>
-              @else
-                  <a href="{{ route('login') }}">Log in</a>
-              @endauth
-          </li>
-          <li class="dropdown d-lg-none">
-            <a class="nav-link scrollto" data-toggle="dropdown" href="#">
-              {{ session('lang') }} <i class="fas fa-globe"></i>
-            </a>
-            <ul>
-              <li><a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a></li>
-              <li><a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a></li>
-            </ul>
-          </li>
-      </nav><!-- .navbar -->
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
-          <li><a class="nav-link scrollto" href="#ptoventa">Punto de venta</a></li>
-          <li><a class="nav-link scrollto" href="#modules">Aplicaciones</a></li>
-          <li><a class="nav-link scrollto" href="#cefa">Map</a></li>
-          <li><a class="nav-link scrollto" href="#why-us">SENA-Empresa</a></li>
-          <li><a class="nav-link scrollto" href="#about">Acerca</a></li>
-          <li><a class="nav-link scrollto" href="#contact">PQRS</a></li>
-          <li class="dropdown">
-              @auth
-                 <a href="{{ route('cefa.home') }}">{{ Auth::user()->nickname }}</a>
-                <ul>
-                  <li><a href="{{ route('password.change.index') }}">Cambiar contraseña</a></li>
-                  <li>
+                </li>
+              </ul>
+            @else
+                <a href="{{ route('login') }}">Log in</a>
+            @endauth
+        </li>
+          
+              <!-- languaje Dropdown Menu-->
+        <li class="dropdown">
+          <a class="nav-link scrollto" data-toggle="dropdown" href="#">
+            {{ session('lang') }} <i class="fas fa-globe"></i>
+          </a>
+          <ul>
+            <li><a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a></li>
+            <li><a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a></li>
+          </ul>
+        </li>
 
-                <a href="{{ route('logout') }}" class="d-block" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">Salir</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                      @csrf
-                </form>
+      </ul>
 
-                  </li>
-                </ul>
-              @else
-                  <a href="{{ route('login') }}">Log in</a>
-              @endauth
-          </li>
-            
-                <!-- languaje Dropdown Menu-->
-          <li class="dropdown">
-            <a class="nav-link scrollto" data-toggle="dropdown" href="#">
-              {{ session('lang') }} <i class="fas fa-globe"></i>
-            </a>
-            <ul>
-              <li><a href="{{ url('lang',['es']) }}" class="dropdown-item scrollto">Español</a></li>
-              <li><a href="{{ url('lang',['en']) }}" class="dropdown-item scrollto">English</a></li>
-            </ul>
-          </li>
+    </nav><!-- .navbar -->
 
-        </ul>
+  </div>
 
-      </nav><!-- .navbar -->
-
-    </div>
-
-  </header><!-- End Header -->
+</header><!-- End Header -->
