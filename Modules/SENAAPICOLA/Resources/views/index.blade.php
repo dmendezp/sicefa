80f693fb62877f6fe3f71ae53e1636642974ca4c
aldemar montenegro <montenegroaldemar1@gmail.com>
2025-03-27 12:09:13 -0500

    Creando Modulo

---
+++ Modules/SENAAPICOLA/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('senaapicola::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('senaapicola.name') !!}
+    </p>
+@endsection
