101a837c0cb99fdae0a47994d60fbf0b5fc60534
juan202006-svg <guzmanhernandezj603@gmail.com>
2025-04-01 09:56:52 -0500

    Creando Modulo

---
+++ Modules/ACUAPONICO/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('acuaponico::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('acuaponico.name') !!}
+    </p>
+@endsection
