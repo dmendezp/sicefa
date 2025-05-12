36742a64c2d47c144450905e6c85e827fdd6e0a7
Estiven9900 <nicolassoriano1404@gmail.com>
2025-05-12 14:59:54 -0500

    Se añadieron las migraciones para las tablas 'apprentice_researchers', 'group' y 'jobs', ajustando las relaciones y eliminando restricciones innecesarias para mejorar la integridad de los datos.

--- Modules/SIA/Database/Migrations/2025_04_25_030233_create_apprentice_researchers_table.php
+++ Modules/SIA/Database/Migrations/2025_04_25_030233_create_apprentice_researchers_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('apprentice_researchers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade'); // Relación uno a uno con users
-            
             $table->foreignId('person_id')->unique()->constrained('people')->onDelete('cascade');
-            $table->foreignId('eps_id')->nullable()->constrained('e_p_s')->onDelete('set null');
+            $table->foreignId('eps_id')->constrained('e_p_s')->onDelete('cascade');
             $table->foreignId('program_id')->constrained('programs')->onDelete('cascade'); 
-            $table->foreignId('course_id')->nullable()->constrained('courses')->onDelete('cascade');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->foreignId('group_id')->constrained('groups')->onDelete('cascade'); 
             $table->foreignId('project_id')->nullable()->constrained('projects')->onDelete('set null'); 
             $table->string('institution', 100)->nullable(); 
             $table->date('start_date'); 
---
+++ Modules/SIA/Database/Migrations/2025_05_12_085330_create_group_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group');
+    }
+}
