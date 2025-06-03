944c6b5ed19dd716afd70b246ded21771f7913e1
Nicolas Estiven Soriano Polania <nicolassoriano1404@gmail.com>
2025-06-03 01:34:38 -0500

    Se reorganizaron las rutas del módulo SIA, añadiendo la ruta para el dashboard del administrador y mejorando la estructura de las rutas para los aprendices investigadores.

--- Modules/SIA/Routes/web.php
+++ Modules/SIA/Routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\SIA\Http\Controllers\SIAController;
+use Modules\SIA\Http\Controllers\Admin\DashboardController;
 use Modules\SIA\Http\Controllers\ApprenticeResearcherController;
 
 /*
@@ -9,33 +10,40 @@ use Modules\SIA\Http\Controllers\ApprenticeResearcherController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Aquí puedes registrar las rutas web para tu aplicación. Estas rutas
-| son cargadas por el RouteServiceProvider dentro de un grupo que
-| contiene el middleware "web".
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::middleware(['lang'])->prefix('sia')->group(function () {
-    // Ruta principal del módulo SIA
+// Agrupar rutas bajo el prefijo 'sia' sin middleware global para permitir acceso público
+Route::prefix('sia')->group(function () {
+    // Ruta principal del módulo SIA (pública, sin autenticación requerida)
     Route::get('/', function () {
         return view('sia::index');
-    })->name('sia.home')->name('home');
+    })->name('sia.home');
 
-    // Ruta adicional para /sia/index
+    // Ruta adicional para /sia/index (pública)
     Route::get('/index', function () {
         return view('sia::index');
     })->name('sia.index');
 
-    // Ruta para la vista de administrador
-    Route::get('/admin', [SIAController::class, 'admin'])->name('sia.admin');
-});
+    // Ruta para información o desarrolladores (pública)
+    Route::get('/developers', [SIAController::class, 'developers'])->name('sia.developers');
 
-Route::prefix('apprentice_researchers')->name('sia.apprentice_researchers.')->middleware(['auth', 'role:administrator'])->group(function () {
-    Route::get('/', [ApprenticeResearcherController::class, 'index'])->name('index');
-    Route::get('/create', [ApprenticeResearcherController::class, 'create'])->name('create');
-    Route::post('/', [ApprenticeResearcherController::class, 'store'])->name('store');
-    Route::get('/{id}', [ApprenticeResearcherController::class, 'show'])->name('show');
-    Route::get('/{id}/edit', [ApprenticeResearcherController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [ApprenticeResearcherController::class, 'update'])->name('update');
-    Route::delete('/{id}', [ApprenticeResearcherController::class, 'destroy'])->name('destroy');
+    // Rutas para administradores (protegidas con autenticación y permisos)
+    Route::prefix('admin')->middleware(['auth', 'permission:sia.admin.index'])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('sia.admin.dashboard');
+    });
+
+    // Rutas para gestión de aprendices investigadores (protegidas con autenticación y rol)
+    Route::prefix('apprentice_researchers')->name('sia.apprentice_researchers.')->middleware(['auth', 'role:sia.admin'])->group(function () {
+        Route::get('/', [ApprenticeResearcherController::class, 'index'])->name('index');
+        Route::get('/create', [ApprenticeResearcherController::class, 'create'])->name('create');
+        Route::post('/', [ApprenticeResearcherController::class, 'store'])->name('store');
+        Route::get('/{id}', [ApprenticeResearcherController::class, 'show'])->name('show');
+        Route::get('/{id}/edit', [ApprenticeResearcherController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [ApprenticeResearcherController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ApprenticeResearcherController::class, 'destroy'])->name('destroy');
+    });
 });
