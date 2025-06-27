4bd6f46a72bbcfaf722bb13d172a88ea6078718d
Nicolas Estiven Soriano Polania <nicolassoriano1404@gmail.com>
2025-06-26 19:02:31 -0500

    Agregar archivos de idioma en español para los módulos SIA, incluyendo traducciones para 'about', 'controllers', 'devs', 'general', y 'mainPage'.

---
+++ Modules/SIA/Resources/lang/es/about.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    // Breadcrumbs
+    'Breadcrumb_Active_About' => 'Acerca de',
+
+    // Card About
+    'Description_About'   => 'Nació en el año 2025, como un módulo de SICEFA, su objetivo es gestionar y administrar el sistema de información del simillero de investigación del centro de formación "La Angostura".',
+    // Card Find Us
+    'Title_Card_Find'     => 'Encuéntranos!',
+    'Text_Card_Find'      => 'Centro de Formación Agroindustrial "La Angostura"',
+    // Card Schedule
+    'Title_Card_Schedule' => 'Horario de Atención',
+    'Text_Card_Schedule'  => 'Lunes - Viernes',
+    // Card Contact
+    'Title_Card_Contact'  => 'Contáctanos',
+    'Text_Card_Contact'   => 'Próximamente',
+    // Card Email
+    'Title_Card_Email'    => 'Correo Electrónico',
+    'Text_Card_Email'     => 'Próximamente',
+
+    // Card Features
+    // Security
+    'Title_Card_Security'    => 'Seguridad',
+    'Text_Card_Security'     => 'Cuenta con un sistema de seguridad óptimo para que la información almacenada sea manejada solo por personal autorizado.',
+    // Efficiently
+    'Title_Card_Efficiently' => 'Eficiente',
+    'Text_Card_Efficiently'  => 'Los procesos se realizan en el menor tiempo posible, garantizando una respuesta rápida a las peticiones.',
+    // Design
+    'Title_Card_Design'      => 'Diseño',
+    'Text_Card_Design'       => 'Presenta un diseño elegante y minimalista, facilitando su uso por parte de los interesados internos sin complicaciones.',
+    // Organized
+    'Title_Card_Organized'   => 'Organizado',
+    'Text_Card_Organized'    => 'Toda la información está al alcance de la mano, organizada de la manera más efectiva.',
+];
---
+++ Modules/SIA/Resources/lang/es/general.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    // Navbar
+    'Home'             => 'Inicio',
+    'admin'            => 'Administrador',
+    'Language'         => 'Internacionalización',
+    'English'          => 'Inglés',
+    'Spanish'          => 'Español',
+    'Full Screen Mode' => 'Modo Pantalla Completa',
+    'Apps'             => 'Aplicaciones',
+    'Log In'           => 'Iniciar Sesión',
+
+    // Navbar MainPage
+    'ModeA'            => 'Modo: Administrador',
+    'ModeU'            => 'Modo: Usuario',
+    'MainPage'         => 'Inicio',
+    'Information'      => 'Información',
+    'AboutUs'          => 'Acerca de',
+    'Developers'       => 'Desarrolladores',
+    'Administration'   => 'Administración',
+    'Projects'         => 'Proyectos',
+    'Resources'        => 'Recursos',
+    'ReportsPanel'     => 'Panel de Reportes',
+    'MovementHistory'  => 'Histórico de Movimientos',
+    'Configuration'    => 'Configuración',
+    'Logout'           => 'Cerrar Sesión',
+    'English'          => 'Inglés',
+    'Spanish'          => 'Español',
+
+    // Sidebar
+    'Session'          => 'Iniciar Sesión',
+    'Back to SICEFA'   => 'Volver a SICEFA',
+    'dashboard'        => 'Panel de control',
+    'Projects'         => 'Proyectos',
+    'Resources'        => 'Recursos',
+    'Reports Panel'    => 'Panel de Reportes',
+    'Movement History' => 'Histórico de Movimientos',
+    'Configuration'    => 'Configuración',
+    'Developers'       => 'Desarrolladores',
+    'About us'         => 'Acerca de',
+
+    // ControlSidebar
+    'Browse'           => 'Navega!',
+    'Clic'             => 'Si quieres ir a otra app solo haz clic.',
+
+    // Footer
+    'All Rights Reserved.' => 'Todos los derechos reservados.',
+
+    // Tooltips
+    'InSession'   => 'Acceder / Ingresar',
+    'ExitSession' => 'Salir',
+];
