0ae8220a4ba71abf0dc2bbea9890d1d023e70518
Nicolas Estiven Soriano Polania <nicolassoriano1404@gmail.com>
2025-07-01 22:29:07 -0500

    Se agregan archivos de traducción en español e inglés para la gestión de grupos, incluyendo títulos, mensajes de éxito/errores y etiquetas de formulario.

---
+++ Modules/SIA/Resources/lang/en/groups.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'index_title_page' => 'List of Groups',
+    'index_title_view' => 'Groups',
+    'create_title_page' => 'Create Group',
+    'create_title_view' => 'Group Registration',
+    'edit_title_page' => 'Edit Group',
+    'edit_title_view' => 'Group Edition',
+    'name' => 'Name',
+    'description' => 'Description',
+    'actions' => 'Actions',
+    'register' => 'Register',
+    'no_records' => 'No groups available',
+    'action_create' => 'Create',
+    'action_edit' => 'Edit',
+    'action_delete' => 'Delete',
+    'action_cancel' => 'Cancel',
+    'action_update' => 'Update',
+    'processing' => 'Processing...',
+    'confirm_delete' => 'Are you sure you want to delete this group?',
+];
